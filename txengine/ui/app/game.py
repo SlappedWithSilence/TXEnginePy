39747d1664020f849c50733dbd9b87fa98281a21
Slapped with Silence <asigal55+02@gmail.com>
2022-05-02 11:52:28 -0500

    Refactored, added text io display

--- txengine/ui/app/game.py
+++ txengine/ui/app/game.py
@@ -1,8 +1,10 @@
 from textual.app import App
 from textual.reactive import Reactive
 from textual.widgets import Footer
+from textual_inputs import IntegerInput
 
-from ..widgets.side_bar import SideBar
+from ..widgets.game_text_output import GameTextOutput
+from ..widgets.side_bar import SummarySideBar, HistorySideBar
 
 
 class Game(App):
@@ -11,6 +13,10 @@ class Game(App):
     # Class Members #
     #################
 
+    # Stores
+    text_history = []
+
+    # Reactives
     show_bar = Reactive(False)
 
     ######################
@@ -20,7 +26,29 @@ class Game(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.side_bar: SideBar = None
+        # Metadata
+        self.root_grid = None
+        self.name = "TXEngine Default Name"
+
+        # Offscreen elements
+        self.summary_side_bar: SummarySideBar = None
+        self.history_side_bar: HistorySideBar = None
+
+        # Text IO elements
+        self.user_input = IntegerInput(name="user_input", title="Input")
+        self.game_text_output = GameTextOutput(name="game_text_output")
+
+    ####################
+    # Helper Functions #
+    ####################
+
+    def __init_text_io(self):
+        self.root_grid.add_column("center")
+        self.root_grid.add_row("output")
+        self.root_grid.add_row("input")
+        self.root_grid.add_areas(input="center,input", output="center,output")
+        self.root_grid.add_widget(self.game_text_output, "output")
+        self.root_grid.add_widget(self.user_input, "input")
 
     #####################
     # Textual Functions #
@@ -29,7 +57,7 @@ class Game(App):
     # Watchers
     def watch_show_bar(self, show_bar: bool) -> None:
         """Called when show_bar changes."""
-        self.side_bar.animate("layout_offset_x", 0 if show_bar else -40)
+        self.summary_side_bar.animate("layout_offset_x", 0 if show_bar else -40)
 
     # Actions
     def action_toggle_sidebar(self) -> None:
@@ -39,23 +67,28 @@ class Game(App):
     # Event
     async def on_load(self) -> None:
         """Bind keys"""
-        await self.bind("b", "toggle_sidebar", "Toggle sidebar")
+        await self.bind("b", "toggle_sidebar", "Toggle summary")
         await self.bind("q", "quit", "Quit")
 
         """Initialize instance variables"""
-        self.side_bar = SideBar()
+        self.summary_side_bar = SummarySideBar()
 
         # TODO: Remove Debug
-        self.side_bar.register_resource("Health")
-        self.side_bar.register_resource("Mana")
+        self.summary_side_bar.register_resource("Health")
+        self.summary_side_bar.register_resource("Mana")
 
     async def on_mount(self) -> None:
         # Set up footer
         footer = Footer()
         await self.view.dock(footer, edge="bottom")
 
+        # Set up text IO
+        self.root_grid = await self.view.dock_grid(edge="top", name="root_grid", gutter=0)
+        self.__init_text_io()
+
         # Set up sidebar
-        await self.view.dock(self.side_bar, edge="left", size=40, z=1)
+        await self.view.dock(self.summary_side_bar, edge="left", size=40, z=1)
 
-        self.side_bar.layout_offset_x = -40
+        self.summary_side_bar.layout_offset_x = -40
 
+        await self.game_text_output.set_content("Some Text", self.name)
