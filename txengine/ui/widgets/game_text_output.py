8c380d9ceba84a70ca3ae3011ea0e5f28499f6da
Slapped with Silence <asigal55+02@gmail.com>
2022-05-02 13:43:04 -0500

    Added logic for displaying option menus

--- txengine/ui/widgets/game_text_output.py
+++ txengine/ui/widgets/game_text_output.py
@@ -1,9 +1,46 @@
+from abc import ABC
 from typing import Union
 
 from rich.console import RenderableType
 from rich.panel import Panel
 from textual.message import Message
 from textual.widget import Widget
+
+from ..color.colors import c_form
+
+class GameTextElement(ABC):
+
+    def __init__(self, source: str = None):
+        self.source: Union[str, None] = source
+
+    def unpack(self) -> str:
+        return ""
+
+
+class MenuElement(GameTextElement):
+
+    def __init__(self,  opts: list[str], source: str = None, header: str = None):
+        super().__init__(source)
+
+        # Check for valid opts
+        if len(opts) < 1:
+            raise ValueError("MenuElement.opts must have length > 1!")
+
+        self.opts: list[str] = opts  # The set of options the player may choose from
+        self.header: [str, None] = header  # The text that may appear over the options
+
+    def unpack(self) -> str:
+
+        base = ""
+
+        if self.header:
+            base = self.header + "\n"
+
+        for i in range(len(self.opts)):
+            opt = c_form(f"[{i}]", "menu_opt_number") + f": " + c_form(self.opts[i], "menu_opt_text")
+            base = base + opt + "\n"
+
+        return base
 
 
 class GameTextChange(Message, bubble=True):
@@ -14,10 +51,20 @@ class GameTextOutput(Widget):
     current_source: str = None
     current_text: str = None
 
-    async def set_content(self, text: str, source: Union[str, None]):
-        self.current_text = text
+    async def set_content(self, content: Union[str, GameTextElement], source: Union[str, None]):
+
+        if self.current_text and self.current_text != "":
+            await self.emit(GameTextChange(self))
+
+        if type(content) == str:
+            self.current_text = content
+        elif isinstance(content, GameTextElement):
+            self.current_text = content.unpack()
+
         self.current_source = source
-        await self.emit(GameTextChange(self))
+
+
+
 
     def render(self) -> RenderableType:
         return Panel(self.current_text or "", title=self.current_source)
