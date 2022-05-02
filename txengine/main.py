97154a430dc5d5fcc67b7e3503143a69874c6ee8
Slapped with Silence <asigal55+02@gmail.com>
2022-05-02 11:51:43 -0500

    Added comment scaffolding

--- txengine/main.py
+++ txengine/main.py
@@ -3,6 +3,12 @@ from ui.app.game import Game
 
 if __name__ == "__main__":
 
+    # Load config
+    pass
+    # Load content
+    pass
+    # Run pre-UI set up
+    pass
     # Create and launch UI
     game = Game()
     game.run()
