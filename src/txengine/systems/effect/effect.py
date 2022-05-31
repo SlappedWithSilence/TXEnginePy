c0cad51c1b1db114ec9841a20876f54c6bb2373d
Slapped with Silence <asigal55+02@gmail.com>
2022-05-31 18:59:35 -0500

    Refactored

--- src/txengine/systems/effect/effect.py
+++ src/txengine/systems/effect/effect.py
@@ -1,9 +1,13 @@
 from abc import ABC
 
+from ..event.events import AbilityEvent
+from ...cache import config
+from ..player import player
+
 
 class Effect(ABC):
     def __init__(self):
-        self.properties: list[str, int, float] = []
+        self.properties: list[str] = []
 
     def perform(self):
         """The heart of the Effect class. This method executes some arbitrary code based on the property values of the
@@ -14,7 +18,7 @@ class Effect(ABC):
         pass
 
 
-class FlatResourceEffect(Effect):
+class FlatStatEffect(Effect):
     """Changes one of the player's combat resources by a flat value
 
     The properties for this effect are [resource_name:str, change_value:int]
@@ -23,11 +27,19 @@ class FlatResourceEffect(Effect):
     def __init__(self):
         super().__init__()
 
+        if len(self.properties) != 2:
+            raise ValueError(f"FlatStatEffect must have exactly two properties! Got {len(self.properties)}")
+
     def perform(self):
-        pass
+        try:
+            stat_name: str = self.properties[0]
+            change_value: int = int(self.properties[1])
+            player.stats[stat_name].adjust(change_value)
+        except TypeError:
+            raise ValueError(f"FlatStatEffect's properties must be typed [str, int]!")
 
 
-class LevelResourceEffect(Effect):
+class LevelStatEffect(Effect):
     """Changes one of the player's combat resources by a value scaled to their level.
 
     The properties for this effect are [resource_name:str, change_value:int, scale_factor:float]
@@ -36,11 +48,23 @@ class LevelResourceEffect(Effect):
     def __init__(self):
         super().__init__()
 
+        if len(self.properties) != 3:
+            raise ValueError(f"LevelStatEffect must have exactly three properties! Got {len(self.properties)}")
+
     def perform(self):
-        pass
+
+        try:
+            stat_name: str = self.properties[0]
+            change_value: int = int(self.properties[1])
+            scale_factor: float = float(self.properties[2])
+
+            adjustment: int = (player.skills[config['primary_skill']].level * scale_factor) + change_value
+            player.stats[stat_name].adjust(int(adjustment))
+        except TypeError:
+            raise ValueError(f"LevelStatEffect's properties must be typed [str, int, float]!")
 
 
-class ProportionalResourceEffect(Effect):
+class ProportionalStatEffect(Effect):
     """Changes one of the player's combat resources scaled to that resource's current value.
 
     The properties for this effect are [resource_name:str, change_percentage:float].
@@ -65,4 +89,5 @@ class TeachAbilityEffect(Effect):
         super().__init__()
 
     def perform(self):
-        pass
+        ae = AbilityEvent([self.properties[0]])
+        ae.perform()
