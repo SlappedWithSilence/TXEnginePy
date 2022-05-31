2f50cc3648556d8c75684d80212801c4c5936ca0
Slapped <asigal55+02@gmail.com>
2022-05-30 19:45:28 -0500

    Added events

--- src/txengine/config/globals.py
+++
@@ -1,4 +0,0 @@
-# Variables
-config: dict = {
-    "debug": False
-}
--- src/txengine/systems/event/events.py
+++ src/txengine/systems/event/events.py
@@ -1,3 +1,7 @@
+from ..flag import flags
+from ...ui.color import style
+from ..item.item_util import item_name
+
 from abc import ABC
 from typing import Union
 
@@ -6,9 +10,9 @@ from rich import print
 
 class Event(ABC):
 
-    def __init__(self, properties: Union[list[str]], text: Union[str, None] = None):
-        self.__properties: list[str] = properties
-        self.__text: Union[str, None] = text
+    def __init__(self, properties: list[str], text: Union[str, None] = None):
+        self._properties: list[str] = properties
+        self._text: Union[str, None] = text
 
     def logic(self) -> None:
         """Core logic for the Event"""
@@ -16,6 +20,83 @@ class Event(ABC):
 
     def perform(self) -> None:
         """Wrapper for logic that prints optional user prompting"""
-        if self.__text:
-            print(self.__text)
+        if self._text:
+            print(self._text)
         self.logic()
+
+
+class FlagEvent(Event):
+    """An event that sets a specific flag to a given value"""
+
+    def __init__(self, properties: list[str]):
+        super().__init__(properties)
+        if len(properties) != 2:
+            raise ValueError(f"FlagEvents must have exactly two properties! Got {len(properties)}")
+
+    def logic(self) -> None:
+
+        try:
+            flag_value = bool(self._properties)
+            flags[self._properties[0]] = flag_value
+
+        except TypeError:  # Any exception from type conversion is really a value error from the JSON files
+            raise ValueError("FlagEvent's properties must be typed [str, bool]!")
+
+
+class AbilityEvent(Event):
+    """Causes the player to learn a given ability"""
+
+    def __init__(self, properties: list[str]):
+        super().__init__(properties)
+        self._text = style("You learned a new ability: ", "player_improvement") + style(properties[0], "ability")
+
+        if len(properties) != 1:
+            raise ValueError(f"AbilityEvent must have exactly one property! Got {len(properties)}")
+
+    # TODO: Implement AbilityEvent logic
+    def logic(self) -> None:
+        # Check if player has ability
+        # if so, set text to reflect redundant learning
+        # if not, learn the ability
+        pass
+
+
+class ItemEvent(Event):
+    """Causes the player to obtain 'n' of a given item"""
+
+    def __init__(self, properties: list[str]):
+        super().__init__(properties)
+
+        if len(properties) != 2:
+            raise ValueError(f"ItemEvent must have exactly two properties! Got {len(properties)}")
+
+        try:
+            self._item_id = int(self._properties[0])
+            self._item_quantity = int(self._properties[1])
+
+        except TypeError:
+            raise ValueError("ItemEvent's properties must be typed [int, int]!")
+
+        self._text = style(f"You obtained {self._item_quantity} ", "player_improvement") + \
+                     style(item_name(self._item_id), "item")
+
+    # TODO: Implement ItemEvent logic
+    def logic(self) -> None:
+        # Get player's inventory
+        # call add_item
+        pass
+
+
+class MoneyEvent(Event):
+
+    def __init__(self, properties: list[str]):
+        super().__init__(properties)
+
+        if len(properties) != 1:
+            raise ValueError(f"Money must have exactly one property! Got {len(properties)}")
+
+    def logic(self) -> None:
+        # If quantity > 0, set gain message
+        # If quantity < 0 set loss message
+        # Set currency
+        pass
