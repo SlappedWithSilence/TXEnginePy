a271a9b5b003153d50e511c5fc6539a1ae49b2ae
SlappedWithSilence <asigal55+02@gmail.com>
2023-01-30 19:51:09 -0800

    Switched to FastAPI

--- src/main.py
+++ src/main.py
@@ -3,24 +3,23 @@ from txengine.systems.room.room import Room
 from txengine.ui.color import init_default_tag_map
 from txengine.cache import config
 
+from fastapi import FastAPI
 
+
+# Define constants
+app_server = FastAPI()
+
+
+# Define helper functions
 def init_debug() -> None:
     """Perform pre-game debugging functions"""
-    acs = []
-    r = Room(0, "The Alley", acs, text="You enter a spooky alleyway.")
-    room_manager.rooms[0] = r
 
 
 def init() -> None:
     """Initialize various engine elements"""
-    init_default_tag_map()
 
 
-if __name__ == "__main__":
-
-    init()
-
-    if config["debug"]:
-        init_debug()
-
-    room_manager.start_room_loop()
+# Define FastAPI functions
+@app_server.get("/")
+async def root():
+    return {"message": "Hello World"}
