7472ce91fc352e5e17657eb59aad0d827ca8626f
SlappedWithSilence <asigal55+02@gmail.com>
2023-02-03 11:22:00 -0800

    Added logic to Room structures

--- src/txengine/systems/room/__init__.py
+++ src/txengine/systems/room/__init__.py
@@ -1,1 +1,19 @@
+class RoomManager:
 
+    def __init__(self):
+        self._current_room_index = 0
+        self._visited_rooms = set()
+
+    def is_visited(self, room_id: int) -> bool:
+        return room_id in self._visited_rooms
+
+    def visit(self, room_id: int) -> None:
+        self._visited_rooms.add(room_id)
+
+    @property
+    def current_room(self) -> int:
+        return self._current_room_index
+
+
+# Define package-level globals
+room_manager = RoomManager()
--- src/txengine/systems/room/room.py
+++ src/txengine/systems/room/room.py
@@ -1,28 +1,27 @@
-from .actiondevice import ActionDevice
+from .action_device import ActionDevice
+from ..managers.engine import Engine
 from ...structures.state_device import StateDevice
-from ...ui.color import style
+from ..room import room_manager
 
 
-class Room (StateDevice):
+class Room(StateDevice):
     """A room represents a scene in TXEngine. The user may interact with a number of functions inside any given room.
 
     A room's contents are defined by which Actions it contains. A room may contain any number of actions. Every room,
     by default, contains some pre-defined set of default actions.
     """
 
-    def __init__(self, room_id: int, name: str, actions: list, on_first_enter_actions=None,
+    def __init__(self, room_id: int, name: str, actions: list, owner: Engine, on_first_enter_actions=None,
                  on_first_enter_text: str = None, text: str = None, ignore_default_actions: bool = False):
         super().__init__()
+        self.name = name
         self.id: int = room_id  # Unique mapping ID
         self.on_first_enter_actions: list = on_first_enter_actions or []  # Actions that trigger only on first enter
         self.actions: list[ActionDevice] = actions  # Actions available for selections
         self.on_first_enter_text: str = on_first_enter_text or ""  # Text that is played only of first enter
         self.ignore_default_actions: bool = ignore_default_actions  # If true, do not show default actions
         self.text: str = text or ""
-
-    def enter(self) -> None:
-        """Enter a loop that only ends when the user selects an ExitAction"""
-        pass
+        self.owner: Engine = owner
 
     @property
     def visible_actions(self) -> list[ActionDevice]:
@@ -30,13 +29,15 @@ class Room (StateDevice):
         return [action for action in self.actions if action.visible]
 
     @property
-    def options(self) -> str:
+    def options(self) -> list[str]:
         """Returns a formatted string containing a numbered menu of actions"""
-        opt_names = [opt.menu_name for opt in self.visible_actions]
-        base = ""
+        return [opt.menu_name for opt in self.visible_actions]
 
-        for index, opt_name in enumerate(opt_names):
-            index_box = f"[{index}]"
-            base = base + style(index_box, "index") + " " + style(opt_name, "listed_option") + "\n"
+    @property
+    def components(self) -> dict[str, any]:
+        return {"content": (self.on_first_enter_text + "\n" if room_manager.is_visited(self.id) else "") + self.text,
+                "options": self.options,
+                }
 
-        return base
+    def _logic(self, user_input: any) -> None:
+        pass
