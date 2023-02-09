c62912150c7523eebccfaaf76a3153d1261d60b3
SlappedWithSilence <asigal55+02@gmail.com>
2023-02-08 16:28:53 -0800

    Switched to synchronous model

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from loguru import logger
 
+from game import engine, state_device_controller
+
 tx_engine = FastAPI()  # FastAPI service object that hosts TXEngine
 
 
 # Implement service logic
 @tx_engine.get("/")
-async def root():
-    return "Implement Me!"
+def root():
+    return state_device_controller.get_current_frame()
+
+
+@tx_engine.put("/")
+def root(user_input: str | int):
+    return state_device_controller.deliver_input(user_input)
 
 
 # Begin service logic
