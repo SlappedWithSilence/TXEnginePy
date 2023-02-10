f2db1a534a4579b4f7ebb4c2249215666084534b
SlappedWithSilence <asigal55+02@gmail.com>
2023-02-10 10:54:27 -0800

    Added type hints

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -1,5 +1,6 @@
 from .game_state_controller import GameStateController
 from .engine import Engine
 
-engine = Engine()
-state_device_controller = GameStateController()
+managers: list = []
+engine: Engine = Engine()
+state_device_controller: GameStateController = GameStateController()
