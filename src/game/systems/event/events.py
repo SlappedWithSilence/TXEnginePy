a4fbf9d1c38eddd1cd954c2d758dbfabb133f2d9
SlappedWithSilence <asigal55+02@gmail.com>
2023-02-12 14:44:43 -0800

    Implemented basic Event subclass logic

--- src/game/systems/event/events.py
+++ src/game/systems/event/events.py
@@ -1,7 +1,8 @@
-from ...structures.enums import InputType
+from ...structures.enums import InputType, affirmative_to_bool
 from ...structures.messages import StringContent
 from ...structures.state_device import StateDevice
 from ...formatting import get_style
+from ..currency import currency_manager
 
 from abc import ABC
 from typing import Union, Any
@@ -17,7 +18,6 @@ class FlagEvent(Event):
     """An event that sets a specific flag to a given value"""
 
     def __init__(self, flags: [tuple[str, bool]]):
-
         super().__init__(input_type=InputType.NONE)
         self.flags = flags  # The flags to set and their corresponding values
 
@@ -26,7 +26,6 @@ class FlagEvent(Event):
         return {}
 
     def _logic(self, _: any) -> None:
-
         for flag in self.flags:
             # TODO: Implement flag setter
             pass
@@ -38,6 +37,9 @@ class AbilityEvent(Event):
     learn_message = [StringContent(value="You learned a new ability!"),
                      StringContent(value="LOOK UP ABILITY NAME", formatting=get_style("ability_name"))
                      ]
+    already_learned_message = [StringContent(value="You already learned "),
+                               StringContent(value="LOOK UP ABILITY NAME", formatting=get_style("ability_name"))
+                               ]
 
     def __init__(self, ability: int):
         super().__init__(input_type=InputType.NONE)
@@ -45,90 +47,105 @@ class AbilityEvent(Event):
 
     @property
     def components(self) -> dict[str, any]:
-
-        return {}
+        return {"content": self.learn_message}  # TODO: Implement already-learned message
 
     # TODO: Implement AbilityEvent logic
     def _logic(self, _) -> None:
+        """
+        Check if the ability has been learned. If it hasn't been learned, learn it
+        """
         # Check if player has ability
         # if so, set text to reflect redundant learning
         # if not, learn the ability
-        pass
+        # TODO: Implement
 
 
 class ItemEvent(Event):
-    """Causes the player to obtain 'n' of a given item"""
+    """
+    Asks the player if they want to accept 'n' of an item. If they do, spawn an item-handling StateDevice
+    """
 
-    def __init__(self, properties: list[str]):
-        super().__init__(properties)
+    def __init__(self, item_id: int, quantity: int):
+        super().__init__(input_type=InputType.AFFIRMATIVE)
+        self.item_id = item_id
+        self.quantity = quantity
 
-        if len(properties) != 2:
-            raise ValueError(f"ItemEvent must have exactly two properties! Got {len(properties)}")
+    def _logic(self, user_input: str) -> None:
+        # TODO: Implement
+        if affirmative_to_bool(user_input):
+            # Spawn item-acquisition StateDevice
+            pass
 
-        try:
-            self._item_id = int(self._properties[0])
-            self._item_quantity = int(self._properties[1])
-
-        except TypeError:
-            raise ValueError("ItemEvent's properties must be typed [int, int]!")
-
-        self._text = style(f"You obtained {self._item_quantity} ", "player_improvement") + \
-                     style(item_name(self._item_id), "item")
-
-    # TODO: Implement ItemEvent logic
-    def logic(self) -> None:
-        # Get player's inventory
-        # call add_item
-        pass
+    @property
+    def components(self) -> dict[str, any]:
+        # TODO: Implement translate item.id to item.name
+        return {"content": [StringContent(value="You've found"),
+                            StringContent(value=str(self.quantity), formatting=get_style("item_quantity")),
+                            StringContent(value=" of "),
+                            StringContent(value=f"item::{self.item_id}", formatting=get_style("item_name")),
+                            StringContent(value=". Do you want to add it to your inventory?")
+                            ]
+                }
 
 
-class MoneyEvent(Event):
+class CurrencyEvent(Event):
 
-    def __init__(self, properties: list[str]):
-        super().__init__(properties)
-
-        if len(properties) != 1:
-            raise ValueError(f"MoneyEvent must have exactly one property! Got {len(properties)}")
+    def __init__(self, currency_id: int | str, quantity: int):
+        super().__init__(input_type=InputType.NONE)
+        self.currency_id = currency_id
+        self.quantity = quantity
+        self.cur = currency_manager.to_currency(currency_id, abs(quantity))
+        self.gain_message: list[StringContent] = [
+            StringContent(value="You gained "),
+            StringContent(value=str(self.cur))
+        ]
+        self.loss_message: list[StringContent] = [
+            StringContent(value="You lost "),
+            StringContent(value=str(self.cur))
+        ]
 
     # TODO: Implement MoneyEvent logic
-    def logic(self) -> None:
+    def _logic(self, _: any) -> None:
         # If quantity > 0, set gain message
         # If quantity < 0 set loss message
         # Set currency
         pass
 
+    @property
+    def components(self) -> dict[str, any]:
+        if self.quantity < 0:
+            msg = self.loss_message
+        else:
+            msg = self.gain_message
+
+        return {"content": msg}
+
 
 class RecipeEvent(Event):
 
-    def __init__(self, properties: list[str]):
-        super().__init__(properties)
-
-        if len(properties) != 1:
-            raise ValueError(f"RecipeEvent must have exactly one property! Got {len(properties)}")
-
-        self._text = style("You learned a new recipe!", "player_improvement")
+    def __init__(self, recipe_id: int):
+        super().__init__(input_type=InputType.NONE)
+        self.recipe_id = recipe_id
 
     # TODO: Implement RecipeEvent logic
-    def logic(self) -> None:
-        # Add given recipe to learned recipes
-        try:
-            recipe_id = int(self._properties[0])
-
-        except TypeError:
-            raise ValueError("RecipeEvent's properties must be typed [int]!")
+    def _logic(self, _: any) -> None:
+        pass
 
 
 class ReputationEvent(Event):
 
-    def __init__(self, properties: list[str]):
-        super().__init__(properties)
-
-        if len(properties) != 3:
-            raise ValueError(f"RecipeEvent must have exactly three properties! Got {len(properties)}")
+    def __init__(self, faction_id: int, reputation_change: int, silent: bool = False):
+        super().__init__(input_type=InputType.SILENT if silent else InputType.NONE)
+        self.faction_id = faction_id
+        self.reputation_change = reputation_change
+        self.message = [StringContent(value="Your reputation with "),
+                        StringContent(value=f"faction::{faction_id}", formatting=get_style("faction_name")),
+                        StringContent(value="decreased" if self.reputation_change < 0 else "increased"),
+                        StringContent(value=f" by {reputation_change}")
+                        ]
 
     # TODO: Implement ReputationEvent logic
-    def logic(self) -> None:
-        self._faction_name = self._properties[0]
+    def _logic(self, _: any) -> None:
         # Parse properties[1] into growth-change enum
         # parse properties[2] into growth-mode enum
         # Calculate total rep change
@@ -136,33 +153,21 @@ class ReputationEvent(Event):
         # Set text
         pass
 
+    @property
+    def components(self) -> dict[str, any]:
+        return {"content": self.message}
+
 
 class StatEvent(Event):
 
-    def __init__(self, properties: list[str]):
-        super().__init__(properties)
+    def __init__(self, stat_name: str, stat_change: int | float):
+        super().__init__(input_type=InputType.NONE)
+        self.stat_name = stat_name
+        self.stat_name: int | float = stat_change
 
-        if len(properties) != 2:
-            raise ValueError(f"StatEvent must have exactly two properties! Got {len(properties)}")
+    def _logic(self, _: any) -> None:
+        pass
 
-    def logic(self) -> None:
-        stat_name = self._properties[0]
-        stat_change: Union[int, float] = None
-
-        # Try to parse data
-        try:
-            stat_change = int(self._properties[1])  # int?
-
-        except TypeError:
-            pass  # Apparently not
-
-        if not stat_change:
-            stat_change = float(self._properties[1])  # float?
-
-        if stat_name not in player.stats:  # Verify stat exists
-            raise ValueError(f"Stat '{stat_name}' does not exist!")
-
-        if not stat_change:
-            raise TypeError("StatEvent's properties must be typed [str, int|float]!")  # still no, something's wrong
-
-        player.stats[stat_name].adjust(stat_change, verbose=True)
+    @property
+    def components(self) -> dict[str, any]:
+        pass
