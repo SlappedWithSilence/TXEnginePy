857ccb4ac0494cf6e0b227ffc25e66f01f1e203e
SlappedWithSilence <asigal55+02@gmail.com>
2023-02-13 13:45:19 -0800

    Refactored, added global cache

--- src/game/cache.py
+++ src/game/cache.py
@@ -1,4 +1,12 @@
+import weakref
+
 config: dict[str, any] = None
+cache: dict[str, any] = {}
+
+
+def get_cache() -> dict:
+    global cache
+    return cache
 
 
 def set_config(cfg: dict) -> None:
