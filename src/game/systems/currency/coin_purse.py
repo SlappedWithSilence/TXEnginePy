135baa95b2f965a7205e22436000b5bb1a3d9358
SlappedWithSilence <asigal55+02@gmail.com>
2023-02-24 16:36:37 -0800

    Added docstrings

--- src/game/systems/currency/coin_purse.py
+++ src/game/systems/currency/coin_purse.py
@@ -22,6 +22,15 @@ class CoinPurse:
         return 0
 
     def spend(self, cur: Currency | int, quantity: int | None = None) -> bool:
+        """
+        Attempt to spend currency.
+
+        Args:
+            cur: the currency to spend, or the id of the currency to spend
+            quantity: The amount of the currency to spend
+
+        Returns: True if the currency was successfully spent, False otherwise
+        """
         if type(cur) == Currency:
             if self.balance(cur) >= cur.quantity:
                 self.currencies[cur.id] = self.currencies[cur.id] - cur
@@ -51,10 +60,28 @@ class CoinPurse:
         return self.currencies[currency_id].quantity >= quantity
 
     def test_all_purchase(self, item_id: int) -> list[int]:
+        """
+        Test if the item with item_id can be purchased using any currency and return a list of valid currency IDs
+
+        Args:
+            item_id: The ID of the item to test against
+
+        Returns: A list of currency IDs that can be used to purchase the item with item IDs
+        """
+
         costs: dict[int, int] = get_cache()['managers']['ItemManager'].get_costs(item_id)
         return [cur_id for cur_id, value in costs.items() if self.test_currency(cur_id, value)]
 
     def test_purchase(self, item_id: int, currency_id: int) -> bool:
+        """
+        Test if the item with id 'item_id' can be purchased using currency with id 'currency_id'.
+
+        Args:
+            item_id: The ID of the item to to test the purchase
+            currency_id: The ID of the currency to use to test
+
+        Returns: True if the there is a sufficient quantity of currency with id 'currency_id', False otherwise
+        """
         return self.test_currency(currency_id, get_cache()['managers']['ItemManager'].get_cost(item_id, currency_id))
 
 
