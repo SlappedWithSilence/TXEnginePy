9731356a34c1b2d04de3200fcf1c8f9997d62e8c
SlappedWithSilence <asigal55+02@gmail.com>
2023-03-16 13:18:24 -0700

    Adjusted formatting

--- test/structures/test_string_content.py
+++ test/structures/test_string_content.py
@@ -2,12 +2,13 @@ import pytest
 
 from game.structures.messages import StringContent
 
+
 def test_trivial_init():
     sc = StringContent(value="A String")
     assert sc.value == "A String"
 
-def test_form_list_init():
 
+def test_form_list_init():
     sc = StringContent(value="A Formatted String", formatting=["Stylish"])
     assert sc.value == "A Formatted String"
     assert type(sc.formatting) == list
