3217210cbe7adbe6d2d951e7f19c44be03869c91
SlappedWithSilence <asigal55+02@gmail.com>
2023-03-17 15:51:25 -0700

    Updated conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,10 @@ A file that contains configuration and utility code pertaining to PyTest
 """
 
 import sys
+
+import pytest
 from loguru import logger
 
-logger.info("Modifying sys.path...")
-sys.path.insert(0, '../src')
+def pytest_configure(config):
+    logger.info("Modifying sys.path...")
+    sys.path.insert(0, 'src')
