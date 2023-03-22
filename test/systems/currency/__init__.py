a45fe3e91a80fd1f42637706f4dd16cc74d2d6b8
SlappedWithSilence <asigal55+02@gmail.com>
2023-03-22 10:36:08 -0700

    Added test item

--- test/systems/currency/__init__.py
+++ test/systems/currency/__init__.py
@@ -11,11 +11,18 @@ def setup_module():
     currency_manager.currencies[-110] = Currency(-110, "USD", {"cents": 1, "dollars": 100})
 
     currency_manager.currencies[-111] = Currency(-111, "Imperial",
-                                              {
-                                                  "bronze": 1,
-                                                  "silver": 1000,
-                                                  "gold": 1000000
-                                              })
+                                                 {
+                                                     "bronze": 1,
+                                                     "silver": 1000,
+                                                     "gold": 1000000
+                                                 })
+
+    from game.systems.item import item_manager, Item
+
+    te = Item("Test Item", -110, {-110: 2, -111: 3}, "A simple test item", 1)
+    item_manager.register_item(te)
+    assert item_manager.get_name(-110) == "Test Item", "Failed to register test item"
+    logger.info(f"Inserted test item: {str(item_manager.get_ref(-110))}")
 
 
 def teardown_module():
@@ -24,3 +31,6 @@ def teardown_module():
     from game.systems.currency import currency_manager
     currency_manager.currencies.pop(-110)
     currency_manager.currencies.pop(-111)
+
+    from game.systems.item import item_manager
+    item_manager._master_item_manifest.pop(-110)
