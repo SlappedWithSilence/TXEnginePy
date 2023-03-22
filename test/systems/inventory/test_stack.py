0ee11580c4b351872a6d2a0667106b5e044751fc
SlappedWithSilence <asigal55+02@gmail.com>
2023-03-22 11:12:36 -0700

    Initial commit

---
+++ test/systems/inventory/test_stack.py
@@ -0,0 +1,14 @@
+from game.systems.inventory.inventory import Stack
+
+
+def test_init():
+    """
+    Test that a Stack object can be trivially initialized under proper conditions
+    """
+    st = Stack(-110, 0)
+
+    assert st.quantity == 0
+    assert st.id == -110
+
+    assert st.ref is not None
+    assert st.ref.id == -110
