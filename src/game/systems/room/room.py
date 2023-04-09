b15d5d6a689a445f13005e413e41e87928b44af9
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-09 11:54:18 -0700

    Update Room to be a child of FiniteStateDevice instead of StateDevice

--- src/game/systems/room/room.py
+++ src/game/systems/room/room.py
@@ -1,35 +1,115 @@
 """
 Contains the main Room object
 """
-import copy
 import weakref
+from enum import Enum
 
 import game
-import game.structures.enums as enums
-import game.structures.state_device as state_device
-import game.systems.room.action.actions as actions
+from game.structures.enums import InputType
+from game.structures.messages import ComponentFactory, StringContent
+from game.structures.state_device import FiniteStateDevice
+
 import game.systems.room as room
+import game.systems.room.action.actions as actions
 
 from loguru import logger
 
 
-class Room(state_device.StateDevice):
+class Room(FiniteStateDevice):
     """
     A StateDevice that simulates a user being inside a "scene" or "room". Rooms act as a container for Actions, of which
     a Room may have many.
     """
-    def __init__(self, id: int, action_list: list[actions.Action], enter_text: str, first_enter_text: str = "", name: str = "Room"):
-        super().__init__(input_type=enums.InputType.INT, name=name)
+
+    class States(Enum):
+        DEFAULT = 0
+        DISPLAY_OPTIONS = 1
+        REQ_MET = 2
+        REQ_NOT_MET = 3
+        LEAVE_ROOM = 4
+        TERMINATE = -1
+
+    def __init__(self, rid: int, action_list: list[actions.Action], enter_text: str, first_enter_text: str = "",
+                 name: str = "Room"):
+        super().__init__(InputType.INT, self.States, self.States.DEFAULT)
 
         self.actions: list[actions.Action] = action_list
         self.enter_text: str = enter_text  # Text that is printed each time room is entered
         self.first_enter_text: str = first_enter_text  # Text only printed the first time the user enters the room
-        self.id: int = id
-        self.domain_min = 0  # The domain min is always 0 for a room.
+        self.id: int = rid
+        self._action_index: int = None
+        self.name = name
 
         # Register self as the owner of each Action
         for action in self.actions:
             action.room = weakref.proxy(self)
+
+        @FiniteStateDevice.state_logic(self, self.States.DEFAULT, InputType.SILENT)
+        def logic(_: any) -> None:
+            self.set_state(self.States.DISPLAY_OPTIONS)
+
+        @FiniteStateDevice.state_content(self, self.States.DEFAULT)
+        def content():
+            return ComponentFactory.get()
+
+        @FiniteStateDevice.state_logic(self, self.States.DISPLAY_OPTIONS, InputType.INT, 0,
+                                       lambda: len(self.options) - 1)
+        def logic(user_input: int) -> None:
+            self._action_index = user_input
+
+            if not self.visible_actions[user_input].is_requirements_fulfilled():
+                logger.warning("Requirements not met!")
+                self.set_state(self.States.REQ_NOT_MET)
+                return
+
+            self.set_state(self.States.REQ_MET)
+
+        @FiniteStateDevice.state_content(self, self.States.DISPLAY_OPTIONS)
+        def content():
+            return ComponentFactory.get(
+                [(self.first_enter_text + "\n" if room.room_manager.is_visited(self.id) else "") + self.enter_text],
+                self.options
+            )
+
+        @FiniteStateDevice.state_logic(self, self.States.REQ_MET, InputType.SILENT)
+        def logic(_) -> None:
+            game.state_device_controller.add_state_device(self.visible_actions[self._action_index])
+
+            if isinstance(self.visible_actions[self._action_index], actions.ExitAction):
+                self.set_state(self.States.LEAVE_ROOM)
+            else:
+                self.set_state(self.States.DISPLAY_OPTIONS)
+
+        @FiniteStateDevice.state_content(self, self.States.REQ_MET)
+        def content():
+            return ComponentFactory.get()
+
+        @FiniteStateDevice.state_logic(self, self.States.REQ_NOT_MET, InputType.ANY)
+        def logic(_) -> None:
+            self.set_state(self.States.DISPLAY_OPTIONS)
+
+        @FiniteStateDevice.state_content(self, self.States.REQ_NOT_MET)
+        def content():
+            return ComponentFactory.get(
+                ["You can't do that!"],
+                self.visible_actions[self._action_index].get_requirements_as_options()
+            )
+
+        @FiniteStateDevice.state_logic(self, self.States.LEAVE_ROOM, InputType.ANY)
+        def logic(_) -> None:
+            self.set_state(self.States.TERMINATE)
+
+        @FiniteStateDevice.state_content(self, self.States.LEAVE_ROOM)
+        def content():
+            return ComponentFactory.get([f"You leave {self.name}"])
+
+        @FiniteStateDevice.state_logic(self, self.States.TERMINATE, InputType.SILENT)
+        def logic(_):
+            game.state_device_controller.set_dead()
+
+        @FiniteStateDevice.state_content(self, self.States.TERMINATE)
+        def content():
+            return ComponentFactory.get()
 
     @property
     def visible_actions(self) -> list[actions.Action]:
@@ -37,31 +117,6 @@ class Room(state_device.StateDevice):
         return [weakref.proxy(action) for action in self.actions if action.visible]
 
     @property
-    def options(self) -> list[str]:
+    def options(self) -> list[list[str | StringContent]]:
         """Returns a formatted string containing a numbered menu of actions"""
-        return [opt.menu_name for opt in self.visible_actions]
-
-    @property
-    def components(self) -> dict[str, any]:
-        """
-        Returns a JSON-formatted dict that contains all necessary text to render the room
-        """
-        # Update domain maximum in case an action was hidden or made visible
-        self.domain_max = len(self.options) - 1
-
-        return {"content": (self.first_enter_text + "\n" if room.room_manager.is_visited(self.id) else "") + self.enter_text,
-                "options": self.options,
-                }
-
-    def _logic(self, user_input: any) -> None:
-
-        # If the user selected and ExitAction, terminate the current room
-        if type(self.visible_actions[user_input] == actions.ExitAction):
-            game.state_device_controller.set_dead()
-            game.state_device_controller.add_state_device(self.visible_actions[user_input])
-
-
-
-
-
-
+        return [[opt.menu_name] for opt in self.visible_actions]
