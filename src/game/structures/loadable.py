03036e1e3fcf989f4a41c463289410a4a6295f1d
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-17 12:21:21 -0700

    Removed attribute checking due to MRO issues

--- src/game/structures/loadable.py
+++ src/game/structures/loadable.py
@@ -7,10 +7,6 @@ class LoadableMixin:
     LOADER_KEY: str = 'loader'
     ATTR_KEY: str = "from_json"
 
-    def __init__(self):
-        if self.ATTR_KEY not in self.__dict__:
-            raise RuntimeError(f"{self.__class__.__name__} is Loadable but does not implement {self.ATTR_KEY}!")
-
     @cached(LOADER_KEY, "LoadableMixin")
     def from_json(self, json: dict[str, any]) -> any:
         raise NotImplementedError()
