5a0925c1daba7ba94134d3e52a9d2ece5cba8229
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-16 19:02:42 -0700

    Exported LoadableMixin's caching logic to decorator, added __init__ attribute introspection

--- src/game/structures/loadable.py
+++ src/game/structures/loadable.py
@@ -1,38 +1,39 @@
-from loguru import logger
+from typing import Callable
 
 from game.cache import get_cache
 
 
-# TODO: Improve Loadable to set loader on import, not on instantiation
+# TODO: Improve Loadable to set loader on definition
+
+
+def cached(root_key: str, attr_key: str) -> Callable:
+
+    def decorate(func: Callable):
+
+        if root_key not in get_cache():
+            get_cache()[root_key] = {}
+
+        if attr_key not in get_cache()[root_key]:
+            get_cache()[root_key][attr_key] = func
+
+        elif get_cache()[root_key][attr_key] != func:
+            raise RuntimeError(f"Cannot cache [{root_key}][{attr_key}]! Something else was already cached!")
+
+        return func
+
+    return decorate
+
 
 class LoadableMixin:
     """An interface for classes that need to define logic for building an instance from JSON"""
 
     LOADER_KEY: str = 'loader'
-    already_assigned: bool = False
+    ATTR_KEY: str = "from_json"
 
     def __init__(self):
-        if not self.already_assigned:
-            cache = get_cache()
+        if self.ATTR_KEY not in self.__dict__:
+            raise RuntimeError(f"{self.__class__.__name__} is Loadable but does not implement {self.ATTR_KEY}!")
 
-            # Build loader cache if it doesn't exist
-            if self.LOADER_KEY not in cache:
-                logger.info("Creating loader cache...")
-                cache[self.LOADER_KEY] = {}
-
-            # If the loader has not already been assigned and doesn't already exist
-            if self.__class__.__name__ not in cache[self.LOADER_KEY]:
-                logger.info(f"Assigning loader function to class {self.__class__.__name__}")
-                cache[self.LOADER_KEY][self.__class__.__name__] = self.from_json
-                self.already_assigned = True
-
-            # If the loader is
-            elif cache[self.LOADER_KEY][self.__class__.__name__] == self.from_json:
-                pass
-            else:
-                raise RuntimeError(f"Cannot assign JSON loader function to class {self.__class__.__name__}! A different "
-                                   f"loader function already exists.")
-
-    @classmethod
-    def from_json(cls, json: dict[str, any]) -> any:
+    @cached(LOADER_KEY, "LoadableMixin")
+    def from_json(self, json: dict[str, any]) -> any:
         raise NotImplementedError()
