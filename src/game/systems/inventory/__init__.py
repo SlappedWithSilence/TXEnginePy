29eee0a3de674f104acaf7c822d3f5ddc77ffbb8
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-18 12:34:25 -0700

    Initial commit

--- src/game/systems/inventory/__init__.py
+++ src/game/systems/inventory/__init__.py
@@ -1,1 +1,4 @@
 from game.systems.inventory.inventory import Inventory, Stack
+from game.systems.inventory.equipment import EquipmentController, EquipmentManager
+
+equipment_manager = EquipmentManager()
