16ac49f71be1838b7c674d04da7bc3feaffe4e6f
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-19 12:20:30 -0700

    Refactored Entity to use Mixin classes

--- src/game/systems/entity/entities.py
+++ src/game/systems/entity/entities.py
@@ -1,21 +1,65 @@
 from abc import ABC
-from dataclasses import dataclass, field
 
 import game.systems.currency.coin_purse
 import game.systems.entity.resource as resource
-import game.systems.inventory.inventory as inv
+import game.systems.inventory.inventory_controller as inv
 
 
-@dataclass
+class InventoryMixin:
+    """
+    A mixin for Entity objects that provides Inventory functionality.
+    """
+
+    def __init__(self, inventory: inv.InventoryController = None, item_manifest: list[tuple[int, int]] = None,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        """
+        An InventoryController's content may be provided via instance, by tuple, or both.
+        """
+        self.inventory = inventory or inv.InventoryController()
+
+        if item_manifest:
+            for item_id, item_quantity in item_manifest:
+                self.inventory.new_stack(item_id, item_quantity, True)
+
+
+class CurrencyMixin:
+    """
+    A mixin for Entity objects that provides CoinPurse functionality.
+    """
+
+    def __init__(self, coin_purse=None, currency_manifest: list[tuple[int, int]] = None,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.coin_purse = coin_purse or game.systems.currency.coin_purse.CoinPurse()
+
+        if currency_manifest:
+            for currency_id, currency_quantity in currency_manifest:
+                self.coin_purse.adjust(currency_id, currency_quantity)
+
+
+class ResourceMixin:
+    """
+    A mixin for Entity objects that provides ResourceController functionality
+    """
+
+    def __init__(self, resource_controller: resource.ResourceController = None,
+                 resource_manifest: list[tuple[str, int, int]] = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.resource_controller: resource.ResourceController = resource_controller or resource.ResourceController()
+
+        if resource_manifest:
+            self.resource_controller = resource.ResourceController(resource_manifest)
+
+
 class Entity(ABC):
     """A basic object that stores an entity's instance attributes such as name, ID, inventory, currencies, etc"""
-    name: str
-    id: int
-    inventory: inv.Inventory = field(default_factory=inv.Inventory)
-    coin_purse: game.systems.currency.coin_purse.CoinPurse = field(default_factory=game.systems.currency.coin_purse.CoinPurse)
-    resources: resource.ResourceController = field(default_factory=resource.ResourceController)
+
+    def __init__(self, name: str, entity_id: int):
+        self.name: str = name
+        self.id: int = entity_id
 
 
-class Player(Entity):
+class Player(ResourceMixin, CurrencyMixin, InventoryMixin, Entity):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(name="Player", entity_id=0, *args, **kwargs)
