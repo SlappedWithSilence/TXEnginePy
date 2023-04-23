8f93725b38be17c6b8bfa915d0f9d06334f9fe3b
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-23 14:03:41 -0700

    Adjusted package-level declarations

--- src/game/systems/entity/__init__.py
+++ src/game/systems/entity/__init__.py
@@ -1,4 +1,4 @@
-import game.systems.entity.entities as entities
+from game.systems.entity.entities import Entity, Player
 from game.systems.entity.resource import Resource
 from game.systems.entity.resource_manager import ResourceManager
 from game.systems.entity.entity_manager import EntityManager
