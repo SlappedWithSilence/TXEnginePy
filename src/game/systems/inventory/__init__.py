1aaa1644191dfd3ed296a1f6d46b34adf387688c
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-23 18:14:29 -0700

    Adjusted package-level declarations

--- src/game/systems/inventory/__init__.py
+++ src/game/systems/inventory/__init__.py
@@ -1,5 +1,5 @@
-from game.systems.inventory.inventory_controller import InventoryController, Stack
 from game.systems.inventory.equipment_controller import EquipmentController
 from game.systems.inventory.equipment_manager import EquipmentManager
+from game.systems.inventory.inventory_controller import InventoryController, Stack
 
 equipment_manager = EquipmentManager()
