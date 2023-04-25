83098a07bd38a5f1042f55968a10bf3f0a5063ed
SlappedWithSilence <asigal55+02@gmail.com>
2023-04-24 21:57:54 -0700

    Implemented LoadableFactory

--- src/game/structures/loadable.py
+++ src/game/structures/loadable.py
@@ -1,4 +1,4 @@
-from game.cache import cached
+from game.cache import cached, get_cache, get_loader
 
 
 class LoadableMixin:
@@ -10,3 +10,28 @@ class LoadableMixin:
     @cached(LOADER_KEY, "LoadableMixin")
     def from_json(self, json: dict[str, any]) -> any:
         raise NotImplementedError()
+
+
+class LoadableFactory:
+
+    @classmethod
+    def get(cls, json: dict[str, any]) -> any:
+        """
+        Instantiate a Loadable object from a JSON blob.
+
+        Args:
+            json: a dict-form representation of a Loadable object
+
+        Returns: An object of the type specified in the JSON.
+        """
+
+        if type(json) != dict:
+            raise TypeError(f"Argument 'json' must be of type dict, got type {type(json)} instead!")
+
+        if "class" not in json:
+            raise ValueError("Cannot load a JSON blob without a class field!")
+
+        if json["class"] not in get_cache()['loader']:
+            raise ValueError(f"No loader for class {json['class']} has been registered!")
+
+        return get_loader(json['class'])(json)
