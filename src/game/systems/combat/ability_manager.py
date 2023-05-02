579aae8a200ada559c8d7b4dc835b5abac6484ed
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-02 12:52:24 -0700

    Added type hints, added logic

--- src/game/systems/combat/ability_manager.py
+++ src/game/systems/combat/ability_manager.py
@@ -1,15 +1,19 @@
 import copy
 
 from game.structures.manager import Manager
+from game.systems.combat.ability import Ability
 
 
 class AbilityManager(Manager):
 
     def __init__(self):
         super().__init__()
-        self._ability_manifest: dict[str, any] = {}
+        self._ability_manifest: dict[str, Ability] = {}
 
-    def get_ability(self, ability_name):
+    def is_ability(self, ability_name: str) -> bool:
+        return ability_name in self._ability_manifest
+
+    def get_ability(self, ability_name) -> Ability:
         if ability_name not in self._ability_manifest:
             raise ValueError(f"{ability_name} is not a valid ability!")
 
