41e2a4056d2bf5572e5e27c3a8f999920615ea99
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-02 13:11:48 -0700

    Added logic

--- src/game/systems/combat/ability_controller.py
+++ src/game/systems/combat/ability_controller.py
@@ -16,14 +16,46 @@ class AbilityController:
                 if not from_cache('managers.AbilityManager').is_ability(ab):
                     raise ValueError(f"{ab} is not a known Ability!")
 
-        self.abilities: list[str] = abilities or []
-
+        self.abilities: set[str] = set(abilities) or set()
 
     def is_learnable(self, ability_name: str) -> bool:
-        pass
+        """
+        Checks if an Ability can be learned by the owning CombatEntity
+
+        args:
+            ability_name: The name of the Ability to check
+
+        Returns:
+            True if the Requirements for the Ability are met by the owning CombatEntity, False otherwise.
+        """
+        return from_cache("managers.AbilityManager").get_ability(ability_name).is_requirements_fulfilled()
 
     def is_learned(self, ability_name: str) -> bool:
-        pass
+        """
+        Checks if the Ability in question has already been learned
 
-    def learn(self, ability_name: str) ->  bool:
-        pass
+        args:
+            ability_name: The name of the Ability to check
+
+        returns:
+            True if the Ability has already been learned.
+        """
+        return ability_name in self.abilities
+
+    def learn(self, ability_name: str) -> bool:
+        """
+        Adds the given ability to the learned abilities set. This does not check if requirements are met!
+
+        In order to add flow to check for requirements, use an AbilityEvent!
+
+        args:
+            ability_name: The name of the Ability to learn
+
+        returns:
+            True if the ability is learned, False otherwise
+        """
+        if not self.is_learned(ability_name):
+            self.abilities.add(ability_name)
+            return True
+
+        return False
