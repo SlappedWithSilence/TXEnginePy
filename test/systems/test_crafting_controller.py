fc18dde5538492e0d6558f366344b6a147aa4c17
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-07 10:31:15 -0700

    Added tests

--- test/systems/test_crafting_controller.py
+++ test/systems/test_crafting_controller.py
@@ -1,8 +1,7 @@
-from game.systems.crafting.crafting_controller import CraftingController
-
 import pytest
 
-from game.systems.entity import Entity
+from game.systems.crafting.crafting_controller import CraftingController
+from game.systems.entity import Player
 from game.systems.inventory import InventoryController
 
 
@@ -27,12 +26,52 @@ def test_manifest(manifest: list[int]):
 
 
 def test_owner():
-    e = Entity(name="Crafty Boy",
+    p = Player(name="Crafty Boy",
                id=1,
                inventory=InventoryController(
-                   items=[
-                    (-110, 1),
-                    (-111, 2)
-                   ]
-                 )
+                   items=[(-110, 1), (-111, 2)]
+               ),
+               recipes=[-110, -111]
                )
+
+    assert hasattr(p, "crafting_controller")
+    assert p.crafting_controller.owner == p
+
+
+def test_has_sufficient_ingredients():
+    p = Player(name="Crafty Boy",
+               id=1,
+               inventory=InventoryController(
+                   items=[(-110, 1), (-111, 2)]
+               ),
+               recipes=[-110, -111]
+               )
+
+    assert p.inventory.total_quantity(-110) == 1
+    assert p.inventory.total_quantity(-111) == 2
+    assert -110 in p.crafting_controller.learned_recipes
+    assert -111 in p.crafting_controller.learned_recipes
+
+    assert p.crafting_controller.has_sufficient_ingredients(-110)
+    assert p.crafting_controller.has_sufficient_ingredients(-111)
+
+
+max_crafts_cases = [
+    [-112, [(-110, 2)], 1],  # Exact
+    [-112, [(-110, 3)], 1],  # Odd offset
+    [-112, [], 0],  # Missing
+    [-112, [(-110, 1)], 0]  # Insufficient
+]
+
+
+@pytest.mark.parametrize("recipe_id, inventory_contents, results", max_crafts_cases)
+def test_max_crafts(recipe_id: int, inventory_contents: list[tuple[int, int]], results: int):
+    p = Player(name="Crafty Boy",
+               id=1,
+               inventory=InventoryController(
+                   items=inventory_contents
+               ),
+               recipes=[-110, -111]
+               )
+
+    assert p.crafting_controller.get_max_crafts(recipe_id) == results
