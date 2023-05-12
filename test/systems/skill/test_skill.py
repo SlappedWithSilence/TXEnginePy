13b18c63d881ca9bc5743d35ec89282d46288511
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-12 11:56:53 -0700

    Added bare cases

--- test/systems/skill/test_skill.py
+++ test/systems/skill/test_skill.py
@@ -13,3 +13,35 @@ def test_init_trivial():
     assert sk.level_up_limit == sk.initial_level_up_limit
     assert sk.remaining_xp == sk.level_up_limit
 
+
+def test_remaining_xp():
+    pass
+
+
+def test_xp_ceiling():
+    pass
+
+
+def test_level_up_trivial():
+    """
+    Tests that level_up correctly handles a single level-up
+    """
+    pass
+
+
+level_up_recursive_cases = [
+
+]
+
+
+@pytest.mark.parametrize("xp", level_up_recursive_cases)
+def test_level_up_recursive(xp: int):
+    """
+    Tests that level_up correctly handles leveling up multiple levels at once
+    """
+    pass
+
+
+def test_gain_xp():
+
+    pass
