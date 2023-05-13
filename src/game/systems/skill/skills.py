c9f6827ccf89b94ac37c03d4b72853b6b62fe611
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-13 11:02:04 -0700

    Refactored code, added helper functions

--- src/game/systems/skill/skills.py
+++ src/game/systems/skill/skills.py
@@ -1,3 +1,4 @@
+import copy
 from abc import ABC
 
 import game
@@ -38,7 +39,30 @@ class SkillBase(ABC):
 
         return _sum
 
-    def _level_up(self):
+    def _trigger_level_up_events(self, level: int) -> None:
+        """
+        For a given level, add all level-up-events to the device stack
+        """
+
+        if type(level) != int:
+            raise TypeError("Level must be an int!")
+
+        if level in self.level_up_events:
+            for event in self.level_up_events[level]:
+                game.state_device_controller.add_state_device(copy.deepcopy(event))
+
+    def force_level_up(self) -> None:
+        """
+        Force a skill to level up regardless of state.
+
+        This increments the level, increments the level_up_limit, and sets XP to 0.
+        """
+        self.level += 1
+        self.xp = 0
+        self.level_up_limit = self._xp_ceiling(self.level)
+        self._trigger_level_up_events(self.level)
+
+    def _check_level_up(self) -> None:
         """
         A helper function that detects if enough xp has been gained to level the skill up. If so, all appropriate events
         are scheduled and the level and level_up fields are updated. If there is enough remaining xp to level up again,
@@ -53,19 +77,16 @@ class SkillBase(ABC):
             self.xp = remaining_xp  # Update xp value to carry-over value
 
             if self.xp >= self.level_up_limit:  # Detect a hanging level up
-                self._level_up()  # Recursively call level_up(). This will make the lowest-level events resolve first.
+                self._check_level_up()  # Recursive call. This will make the lowest-level events resolve first.
 
-            # Add all level-up-events to the game-state-controller's stack
-            if self.level in self.level_up_events:
-                for event in self.level_up_events[self.level]:
-                    game.state_device_controller.add_state_device(event)
+            self._trigger_level_up_events(self.level)
 
     def gain_xp(self, xp: int) -> None:
         """
         Add XP to the skill. This will automatically trigger a level-up event if necessary.
         """
         self.xp += xp
-        self._level_up()
+        self._check_level_up()
 
     @property
     def remaining_xp(self) -> int:
