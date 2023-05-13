61c275596dcbe6a4bf3340628416bcfdcd2dd6ca
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-13 11:01:36 -0700

    Added requirement skeletons

--- src/game/systems/requirement/requirements.py
+++ src/game/systems/requirement/requirements.py
@@ -2,11 +2,12 @@ from abc import ABC
 
 from loguru import logger
 
-from game.structures.loadable import LoadableFactory
+from game.cache import from_cache
+from game.structures.loadable import LoadableFactory, LoadableMixin
 from game.structures.messages import StringContent
 
 
-class Requirement(ABC):
+class Requirement(LoadableMixin, ABC):
     """
     An abstract object that defines broad logical parameters that must be met. Children of this class narrow that logic
     and allow other objects to compose them to enforce a wide variety of gameplay mechanics.
@@ -92,3 +93,73 @@ class RequirementsMixin:
         return requirements
 
 
+class SkillRequirement(Requirement):
+    """
+    Fulfilled when the target has a skill level >= the specified level.
+    """
+
+    def fulfilled(self, entity) -> bool:
+        pass
+
+    @property
+    def description(self) -> list[str | StringContent]:
+        pass
+
+    @staticmethod
+    def from_json(json: dict[str, any]) -> any:
+        pass
+
+
+class ResourceRequirement(Requirement):
+    """
+    Fulfilled when the target entity has >= the specified quantity of a given resource
+    """
+
+    def fulfilled(self, entity) -> bool:
+        pass
+
+    @property
+    def description(self) -> list[str | StringContent]:
+        pass
+
+    @staticmethod
+    def from_json(json: dict[str, any]) -> any:
+        pass
+
+
+class ConsumeResourceRequirement(Requirement):
+    """
+    Fulfilled when the specified quantity of a resource is successfully consumed.
+    """
+    def fulfilled(self, entity) -> bool:
+        pass
+
+    @property
+    def description(self) -> list[str | StringContent]:
+        pass
+
+    @staticmethod
+    def from_json(json: dict[str, any]) -> any:
+        pass
+
+
+class FlagRequirement(Requirement):
+    """
+    Fulfilled when the designated flag is set to True.
+    """
+
+    def __init__(self, flag_name: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.flag = flag_name
+
+    def fulfilled(self, entity) -> bool:
+        fm = from_cache("managers.FlagManager")
+        return fm.get_flag(self.flag)
+
+    @property
+    def description(self) -> list[str | StringContent]:
+        pass
+
+    @staticmethod
+    def from_json(json: dict[str, any]) -> any:
+        pass
