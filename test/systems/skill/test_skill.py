40b327b3aa3dc3b8119f4f0ec3b631bb542e0c53
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-18 11:40:44 -0700

    Added test logic

--- test/systems/skill/test_skill.py
+++ test/systems/skill/test_skill.py
@@ -15,33 +15,58 @@ def test_init_trivial():
 
 
 def test_remaining_xp():
-    pass
+    sk = Skill(name="SomeSkill", id=0)
+    assert sk.remaining_xp == sk.level_up_limit - sk.xp
+
+    for i in range(sk.remaining_xp):
+        sk.xp += 1
+        assert sk.remaining_xp == sk.level_up_limit - sk.xp
 
 
 def test_xp_ceiling():
-    pass
+    sk = Skill(name="SomeSkill", id=0)
+    sk.level_up_limit = sk.initial_level_up_limit = 4
+    sk.next_level_ratio = 1.5
+
+    assert sk._xp_ceiling(1) == 4
+    assert sk._xp_ceiling(2) == 6
+    assert sk._xp_ceiling(3) == 9
+
+    sk.force_level_up()
+    assert sk.level_up_limit == 6
 
 
 def test_level_up_trivial():
     """
     Tests that level_up correctly handles a single level-up
     """
-    pass
+    sk = Skill(name="SomeSkill", id=0)
+    sk.level_up_limit = sk.initial_level_up_limit = 4
+    sk.next_level_ratio = 1.5
+
+    sk.gain_xp(4)
+
+    assert sk.level == 2
+    assert sk.xp == 0
+    assert sk.level_up_limit == 6
 
 
 level_up_recursive_cases = [
-
+    [6, 2, 2],
+    [10, 3, 0],
+    [40, 5, 7]
 ]
 
 
-@pytest.mark.parametrize("xp", level_up_recursive_cases)
-def test_level_up_recursive(xp: int):
+@pytest.mark.parametrize("xp, expected_level, leftover_xp", level_up_recursive_cases)
+def test_level_up_recursive(xp: int, expected_level: int, leftover_xp: int):
     """
     Tests that level_up correctly handles leveling up multiple levels at once
     """
-    pass
+    sk = Skill(name="SomeSkill", id=0)
+    sk.level_up_limit = sk.initial_level_up_limit = 4
+    sk.next_level_ratio = 1.5
 
-
-def test_gain_xp():
-
-    pass
+    sk.gain_xp(xp)
+    assert sk.level == expected_level
+    assert sk.xp == leftover_xp
