9a93a6bdad8c7dc54dc485270ba2d34275ae034c
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-18 11:21:16 -0700

    Initial commit

---
+++ src/game/systems/faction/faction.py
@@ -0,0 +1,79 @@
+from abc import ABC
+
+from game.structures.loadable import LoadableMixin, LoadableFactory
+
+
+class FactionBase(ABC):
+    """
+    A Faction represents a thematic group set within a game world. A faction's affinity represents how positively or
+    negatively that faction feels towards the player.
+
+    Faction affinity may be increased or decreased via Events and can be checked via Requirements.
+    """
+
+    def __init__(self, name: str, id: int, tags: list[str] = None, affinity: int = 0):
+        self.name = name
+        self.id = id
+        self.tags = tags or []
+        self._affinity = affinity
+
+    @property
+    def affinity(self) -> int:
+        return self._affinity
+
+    @affinity.setter
+    def affinity(self, value: int) -> None:
+        assert type(value) == int
+        self._affinity = value
+
+    def adjust_affinity(self, quantity: int | float) -> None:
+        """
+        Adjust the value of the Faction's affinity.
+
+        If an int is passed, adjust by a flat value. If a float is passed, adjust by a percentage where
+        % change = quantity * 100
+
+        Args:
+            quantity: The quantity by which to adjust affinity
+
+        Returns: None
+        """
+        if type(quantity) == int:
+            self._affinity += quantity
+        elif type(quantity) == float:
+            self._affinity += self._affinity * quantity
+        else:
+            raise TypeError(f"Cannot adjust affinity by type {type(quantity)} Must be an int or float!")
+
+
+class Faction(LoadableMixin, FactionBase):
+
+    def __init__(self, name: str, id: int, tags: list[str] = None, affinity: int = 0, **kwargs):
+        super().__init__(name=name, id=id, tags=tags, affinity=affinity, **kwargs)
+
+    @staticmethod
+    def from_json(json: dict[str, any]) -> any:
+        """
+        Loads a Faction object from a JSON blob.
+
+        Required JSON fields:
+        name: (str)
+        id: (int)
+        tags: [str]
+        affinity: (int)
+        """
+
+        required_fields = [
+            ("name", str),
+            ("id", int),
+            ("tags", list),
+            ("affinity", int)
+        ]
+
+        LoadableFactory.validate_fields(required_fields, json)
+
+        if json['class'] != "Faction":
+            raise ValueError()
+
+        return Faction(json['name'], json['id'], json['tags'], json['affinity'])
+
---
+++ src/game/systems/faction/faction_manager.py
@@ -0,0 +1,49 @@
+from game.structures.manager import Manager
+from game.systems.faction.faction import Faction
+
+
+class FactionManager(Manager):
+
+    def __init__(self):
+        super().__init__()
+        self._faction_manifest: dict[int, Faction] = {}
+
+    def __contains__(self, item) -> bool:
+        return self._faction_manifest.__contains__(item)
+
+    def __getitem__(self, item) -> Faction:
+        return self._faction_manifest.__getitem__(item)
+
+    def get_affinity(self, faction_id: int) -> int:
+        """
+        Retrieve the affinity for a given faction
+        """
+        if faction_id not in self:
+            raise ValueError(f"Unknown faction with id: {faction_id}")
+
+        return self[faction_id].affinity
+
+    def adjust_affinity(self, faction_id: int, quantity: int | float) -> None:
+        """
+        Adjust the affinity for a given faction.
+        """
+        if faction_id not in self:
+            raise ValueError(f"Unknown faction with id: {faction_id}")
+
+        self[faction_id].adjust_affinity(quantity)
+
+    def register_faction(self, faction: Faction) -> None:
+        """
+        Register a Faction with the manager.
+        """
+
+        if faction.id in self:
+            raise ValueError(f"Cannot register faction {faction.name}! A faction with id {faction.id} already exists!")
+
+        self._faction_manifest[faction.id] = faction
+
+    def load(self) -> None:
+        pass
+
+    def save(self) -> None:
+        pass
