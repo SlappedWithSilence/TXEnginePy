3d3f0fad27674ba68c262ef1225e609746a9d994
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-21 13:57:58 -0700

    Adjusted imports

--- src/game/systems/combat/combat_engine/combat_engine.py
+++ src/game/systems/combat/combat_engine/combat_engine.py
@@ -1,9 +1,8 @@
-import weakref
-from abc import ABC
+from __future__ import annotations
 
-from game.systems.combat.combat_engine.phase_handler import PhaseHandler
-from game.systems.entity.entities import CombatEntity
 from game.structures.enums import CombatPhase
+from game.systems.combat.combat_engine.phase_handler import PhaseHandler
+import game.systems.entity.entities as entities
 
 
 class CombatEngine:
@@ -24,11 +23,11 @@ class CombatEngine:
 
     def __init__(self):
         # Master collections
-        self._allies: list[CombatEntity] = []  # Master list of allied entities
-        self._enemies: list[CombatEntity] = []  # Master list of opposed entities
+        self._allies: list[entities.CombatEntity] = []  # Master list of allied entities
+        self._enemies: list[entities.CombatEntity] = []  # Master list of opposed entities
 
         # Master ordered lists
-        self._turn_order: list[CombatEntity] = []  # Proxied list of all entities arranged by index via turn order
+        self._turn_order: list[entities.CombatEntity] = []  # Proxied list of all entities arranged by index via turn order
         self._PHASE_ORDER = tuple(CombatEngine.get_master_phase_order())  # Ordered immutable collection of phases
 
         # State data for current turn
@@ -38,7 +37,11 @@ class CombatEngine:
     def _handle_phase(self) -> None:
         """
         Dispatch handling of the current phase to a new instance of a designated handler.
+
+        Each type that is mapped to the current combat phase is instantiated statelessly and passed a reference to self.
+        Handlers are instantiated and activated in the order that they are stored in the PHASE_HANDLERS dict.
         """
+
         for handler_class in self.PHASE_HANDLERS[self.current_phase]:
             handler: PhaseHandler = handler_class()
             handler.handle_phase(self)
