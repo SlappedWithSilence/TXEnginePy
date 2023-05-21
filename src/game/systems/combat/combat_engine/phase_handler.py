e603544e13490bad1f4a405793cbcfa5bea036be
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-21 13:59:38 -0700

    Added a handler

--- src/game/systems/combat/combat_engine/phase_handler.py
+++ src/game/systems/combat/combat_engine/phase_handler.py
@@ -8,3 +8,15 @@ class PhaseHandler(ABC):
 
     def handle_phase(self, combat_engine) -> None:
         raise NotImplementedError()
+
+
+class EffectActivator(PhaseHandler):
+    """
+    For the active entity, activate all Effects that are stored in it for the current Phase.
+    """
+
+    def handle_phase(self, combat_engine) -> None:
+        active_entity = combat_engine.turn_order[combat_engine.current_turn]
+
+        for effect in active_entity.active_effects[combat_engine.current_phase]:
+            effect.perform()  # Assumes that the effect has already been assigned
