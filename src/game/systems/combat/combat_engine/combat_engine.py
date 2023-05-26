cb0951d91eaece0f1f6491f1b55b386565ae02ce
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-26 10:28:38 -0700

    Added phase handlers, added logic for generating instances of entities, added turn-order logic

--- src/game/systems/combat/combat_engine/combat_engine.py
+++ src/game/systems/combat/combat_engine/combat_engine.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
 
+import weakref
+
+from game.cache import from_cache
 from game.structures.enums import CombatPhase
-from game.systems.combat.combat_engine.phase_handler import PhaseHandler
+from game.systems.combat.combat_engine.phase_handler import PhaseHandler, EffectActivator
 import game.systems.entity.entities as entities
 
 
 class CombatEngine:
+
     PHASE_HANDLERS: dict[CombatPhase, list[type]] = {
-        CombatPhase.START_PHASE: [],
-        CombatPhase.PRE_ACTION_PHASE: [],
+        CombatPhase.START_PHASE: [EffectActivator],
+        CombatPhase.PRE_ACTION_PHASE: [EffectActivator],
         CombatPhase.ACTION_PHASE: [],
-        CombatPhase.POST_ACTION_PHASE: [],
-        CombatPhase.END_PHASE: []
+        CombatPhase.POST_ACTION_PHASE: [EffectActivator],
+        CombatPhase.END_PHASE: [EffectActivator]
     }
 
     @classmethod
@@ -21,13 +25,15 @@ class CombatEngine:
             CombatPhase.POST_ACTION_PHASE, CombatPhase.END_PHASE
         ]
 
-    def __init__(self):
+    def __init__(self, ally_entity_ids: list[int], enemy_entity_ids: list[int]):
         # Master collections
-        self._allies: list[entities.CombatEntity] = []  # Master list of allied entities
-        self._enemies: list[entities.CombatEntity] = []  # Master list of opposed entities
+        entity_manager = from_cache("managers.EntityManager")
+        self._allies: list[entities.CombatEntity] = [entity_manager[e_id] for e_id in ally_entity_ids]  # Master list of allied _entity_manifest
+        self._enemies: list[entities.CombatEntity] = [entity_manager[e_id] for e_id in enemy_entity_ids]  # Master list of opposed _entity_manifest
+        self._player_ref: entities.Player = from_cache('player')
 
         # Master ordered lists
-        self._turn_order: list[entities.CombatEntity] = []  # Proxied list of all entities arranged by index via turn order
+        self._turn_order: list[entities.CombatEntity] = []  # Proxied list of all _entity_manifest arranged by index via turn order
         self._PHASE_ORDER = tuple(CombatEngine.get_master_phase_order())  # Ordered immutable collection of phases
 
         # State data for current turn
@@ -47,4 +53,13 @@ class CombatEngine:
             handler.handle_phase(self)
 
     def _compute_turn_order(self):
-        pass
+        """
+        Determine turn order by sorting all entities via turn_speed
+        """
+
+        # Build the weakref list if it doesn't exist
+        if self._turn_order is None or len(self._turn_order) == 0:
+            self._turn_order = [weakref.proxy(e) for e in self._allies + self._enemies]
+
+        # Sort in place
+        self._turn_order.sort(key=lambda x: x.turn_speed)
