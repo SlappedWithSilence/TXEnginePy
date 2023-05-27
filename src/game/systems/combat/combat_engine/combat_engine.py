0b5bb0357f575c5c48dbf217dc6c5c7c7de8021a
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-27 11:25:42 -0700

    Fixed poor typing for CombatEntities. Switched to a FiniteStateDevice base class. Added states.

--- src/game/systems/combat/combat_engine/combat_engine.py
+++ src/game/systems/combat/combat_engine/combat_engine.py
@@ -1,22 +1,32 @@
 from __future__ import annotations
 
 import weakref
+from enum import Enum
 
-from game.cache import from_cache
-from game.structures.enums import CombatPhase
-from game.systems.combat.combat_engine.phase_handler import PhaseHandler, EffectActivator
 import game.systems.entity.entities as entities
+from game.cache import from_cache
+from game.structures.enums import CombatPhase, InputType
+from game.structures.messages import ComponentFactory
+from game.structures.state_device import FiniteStateDevice
+from game.systems.combat.combat_engine.phase_handler import PhaseHandler, EffectActivator
 
 
-class CombatEngine:
-
-    PHASE_HANDLERS: dict[CombatPhase, list[type]] = {
+class CombatEngine(FiniteStateDevice):
+    PHASE_HANDLERS: dict[CombatPhase, list[type[PhaseHandler]]] = {
         CombatPhase.START_PHASE: [EffectActivator],
         CombatPhase.PRE_ACTION_PHASE: [EffectActivator],
         CombatPhase.ACTION_PHASE: [],
         CombatPhase.POST_ACTION_PHASE: [EffectActivator],
         CombatPhase.END_PHASE: [EffectActivator]
     }
+
+    class States(Enum):
+        DEFAULT = 0
+        HANDLE_PHASE = 1
+        DETECT_COMBAT_TERMINATION = 2
+        PLAYER_LOSS = 3
+        PLAYER_VICTORY = 4
+        TERMINATE = -1
 
     @classmethod
     def get_master_phase_order(cls) -> list[CombatPhase]:
@@ -26,19 +36,26 @@ class CombatEngine:
         ]
 
     def __init__(self, ally_entity_ids: list[int], enemy_entity_ids: list[int]):
+        super().__init__(InputType.ANY, self.States, self.States.DEFAULT)
+
         # Master collections
         entity_manager = from_cache("managers.EntityManager")
-        self._allies: list[entities.CombatEntity] = [entity_manager[e_id] for e_id in ally_entity_ids]  # Master list of allied _entity_manifest
-        self._enemies: list[entities.CombatEntity] = [entity_manager[e_id] for e_id in enemy_entity_ids]  # Master list of opposed _entity_manifest
+        self._allies: list[entities.CombatEntity] = [entity_manager[e_id] for e_id in
+                                                     ally_entity_ids]  # Master list of allied _entity_manifest
+        self._enemies: list[entities.CombatEntity] = [entity_manager[e_id] for e_id in
+                                                      enemy_entity_ids]  # Master list of opposed _entity_manifest
         self._player_ref: entities.Player = from_cache('player')
 
         # Master ordered lists
-        self._turn_order: list[entities.CombatEntity] = []  # Proxied list of all _entity_manifest arranged by index via turn order
+        self._turn_order: list[
+            entities.CombatEntity] = []  # Proxied list of all entities arranged by index via turn order
         self._PHASE_ORDER = tuple(CombatEngine.get_master_phase_order())  # Ordered immutable collection of phases
 
         # State data for current turn
         self.current_turn: int = 0
         self.current_phase: CombatPhase = CombatPhase.START_PHASE
+
+        self._build_states()
 
     def _handle_phase(self) -> None:
         """
@@ -63,3 +80,65 @@ class CombatEngine:
 
         # Sort in place
         self._turn_order.sort(key=lambda x: x.turn_speed)
+
+    def _handle_use_item(self, item_id: int) -> None:
+        """
+        The active entity has chosen to ue an Item. Handle its usage.
+        """
+        pass
+
+    def _handle_use_ability(self, ability_name: str) -> None:
+        """
+        The active entity has chosen to use an Ability. Handle its usage.
+        """
+        pass
+
+    def _handle_pass_turn(self) -> None:
+        """
+        The active entity has chosen to not make an action during its turn.
+        """
+        pass
+
+    @property
+    def active_entity(self) -> entities.Entity:
+        """
+        Return a weakref to the active entity.
+        """
+
+        return self._turn_order[self.current_turn]
+
+    def handle_turn_action(self, choice: int | str | None) -> None:
+        """
+        Perform the logic for executing the choice made by the active entity.
+        """
+
+        if type(choice) == int:
+            self._handle_use_item(choice)
+        elif type(choice) == str:
+            self._handle_use_item(choice)
+        elif choice is None:
+            self._handle_pass_turn()
+        else:
+            raise TypeError(f"Unexpected type for choice! Expected str, int, None, got {type(choice)} instead!")
+
+    def _build_states(self) -> None:
+        """
+        Build the state logic and content providers for the FiniteStateDevice functionality of the CombatEngine.
+        """
+        @FiniteStateDevice.state_logic(self, self.States.DEFAULT, InputType.SILENT)
+        def logic(_: any) -> None:
+
+            self.set_state(self.States.HANDLE_PHASE)
+
+        @FiniteStateDevice.state_content(self, self.States.DEFAULT)
+        def content():
+            return ComponentFactory.get()
+
+        @FiniteStateDevice.state_logic(self, self.States.HANDLE_PHASE, InputType.SILENT)
+        def logic(_: any) -> None:
+            for handler_cls in self.PHASE_HANDLERS[self.current_phase]:
+                handler_cls().handle_phase(self)
+
+        @FiniteStateDevice.state_content(self, self.States.HANDLE_PHASE)
+        def content():
+            return ComponentFactory.get()
