1f10d501e5cb4bac32858c791ffb32b8cdb9c737
SlappedWithSilence <asigal55+02@gmail.com>
2023-05-27 11:27:46 -0700

    Added error-checking wrapper to PhaseHandler logic. Fixed EffectActivator logic (now adds Effects to StateDevice stack instead of directly executing logic).

--- src/game/systems/combat/combat_engine/phase_handler.py
+++ src/game/systems/combat/combat_engine/phase_handler.py
@@ -1,22 +1,54 @@
 from abc import ABC
 
+import game
+import game.systems.entity.entities as entities
+
 
 class PhaseHandler(ABC):
 
     def __init__(self):
         pass
 
-    def handle_phase(self, combat_engine) -> None:
+    def _phase_logic(self, combat_engine) -> None:
+        """
+        Inner logic for executive phase handling.
+        """
         raise NotImplementedError()
+
+    def handle_phase(self, combat_engine) -> None:
+        """
+        Wrapper for _phase_logic that provides consistent stateless error checking against the combat_engine.
+        """
+
+        if combat_engine.active_entity is None or not isinstance(combat_engine.active_entity, entities.CombatEntity):
+            raise TypeError(f"Active entity must be instance of CombatEntity! Got {type(combat_engine.active_entity)} instead!")
+
+        self._phase_logic(combat_engine)
 
 
 class EffectActivator(PhaseHandler):
     """
     For the active entity, activate all Effects that are stored in it for the current Phase.
+
+    Since Effects are StateDevices, put them on the StateDevice stack and execute them state-wise. The LAST-CREATED
+    EFFECT is resolved first (LIFO order).
     """
 
-    def handle_phase(self, combat_engine) -> None:
-        active_entity = combat_engine.turn_order[combat_engine.current_turn]
+    def _phase_logic(self, combat_engine) -> None:
+        active_entity = combat_engine.active_entity
 
-        for effect in active_entity.active_effects[combat_engine.current_phase]:
-            effect.perform()  # Assumes that the effect has already been assigned
+        for effect in reversed(active_entity.active_effects[combat_engine.current_phase]):
+            game.state_device_controller.add_state_device(effect)
+
+
+class ChoiceActivator(PhaseHandler):
+    """
+    For the active entity, compute that entity's turn choice.
+    """
+
+    def _phase_logic(self, combat_engine) -> None:
+        """
+        Compute the choice made by the active entity and pass it back to the combat_engine instance to handle execution.
+        """
+
+        combat_engine.handle_turn_action(combat_engine.active_entity.make_choice())
