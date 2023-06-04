f189c8c07cbab16b29d2109082ee443fba8842ac
SlappedWithSilence <asigal55+02@gmail.com>
2023-06-04 12:12:33 -0700

    Added loading code

--- src/game/systems/currency/currency_manager.py
+++ src/game/systems/currency/currency_manager.py
@@ -1,7 +1,9 @@
 import copy
 
 from game.structures import manager as manager
+from game.structures.loadable_factory import LoadableFactory
 from game.systems.currency import Currency
+from game.util.asset_utils import get_asset
 
 
 class CurrencyManager(manager.Manager):
@@ -9,6 +11,12 @@ class CurrencyManager(manager.Manager):
     An object that manages TXEngine's Currency system. This includes loading and saving currency asset definitions,
     validating various currency conversions, and more.
     """
+
+    CURRENCY_ASSET_PATH = "currencies"
+
+    def __init__(self):
+        super().__init__()
+        self._manifest: dict[int, Currency] = {}
 
     def __contains__(self, item: int | Currency):
         if type(item) == int:
@@ -20,16 +28,6 @@ class CurrencyManager(manager.Manager):
 
     def __iter__(self):
         return self._manifest.values().__iter__()
-
-    def load(self) -> None:
-        pass
-
-    def save(self) -> None:
-        pass
-
-    def __init__(self):
-        super().__init__()
-        self._manifest: dict[int, Currency] = {}
 
     def to_currency(self, currency_id: int, quantity: int) -> Currency:
         """
@@ -49,3 +47,17 @@ class CurrencyManager(manager.Manager):
             raise ValueError(f"Currency with id {currency.id} already exists!")
 
         self._manifest[currency.id] = currency
+
+    def load(self) -> None:
+        raw_asset = get_asset(self.CURRENCY_ASSET_PATH)
+
+        for raw_currency in raw_asset['content']:
+            currency = LoadableFactory.get(raw_currency)
+
+            if not isinstance(currency, Currency):
+                raise TypeError(f"Expected object of type Currency, got type {type(currency)} instead!")
+
+            self.register_currency(currency)
+
+    def save(self) -> None:
+        pass
