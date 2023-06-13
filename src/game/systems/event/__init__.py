c6c5fdfc538e8f11c8ef8fd620abd3c6fd811f1e
SlappedWithSilence <asigal55+02@gmail.com>
2023-06-13 12:35:02 -0700

    Imported events at package level to trigger from_json caching

---
+++ src/game/systems/event/__init__.py
@@ -0,0 +1,4 @@
+from game.systems.event.events import Event, FlagEvent, CurrencyEvent, ReputationEvent, ResourceEvent, LearnAbilityEvent, LearnRecipeEvent, ConsumeItemEvent
+from game.systems.event.add_item_event import AddItemEvent
+from game.systems.event.crafting_event import CraftingEvent
+from game.systems.event.use_item_event import UseItemEvent
