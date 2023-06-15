f5b23a33ca86927b816b4b4b8f7f21d56a824dc6
SlappedWithSilence <asigal55+02@gmail.com>
2023-06-15 11:20:10 -0700

    Temp fix for weird input_type typing behavior

--- src/viewer/viewer.py
+++ src/viewer/viewer.py
@@ -3,6 +3,7 @@ from rich import print
 from textual.app import App, ComposeResult
 from textual.reactive import Reactive
 from textual.widgets import Header, Footer, Static
+from loguru import logger
 
 
 def formatting_to_tags(tags: list[str], opening_tag: bool = None, closing_tag: bool = None) -> str:
@@ -16,6 +17,10 @@ def formatting_to_tags(tags: list[str], opening_tag: bool = None, closing_tag: b
             buf = buf + f"[/{tag}]"
 
     return buf
+
+
+def format_string(content: str, tags: list[str]) -> str:
+    return formatting_to_tags(tags, opening_tag=True) + content + formatting_to_tags(tags, closing_tag=True)
 
 
 def parse_content(content: list) -> str:
@@ -38,10 +43,48 @@ class Viewer:
 
     def __init__(self):
         u = input("Enter the IP for the TXEngine server: ")
-        self.ip = 'http://' + (u if u != "" else "localhost:8000")
-        self.session = requests.Session()
+        self._ip = 'http://' + (u if u != "" else "localhost:8000")
+        self._session = requests.Session()
 
-    def display(self, value: dict):
+    def start_session(self):
+        """
+        Start the core loop for getting/put API calls.
+        """
+
+        while True:
+            results = self._session.get(self._ip, verify=False)
+            self.display(results.json())
+            user_input = input()
+
+            self._session.put(self._ip, params={"user_input": user_input}, verify=False)
+
+    def get_text_header(self, tx_engine_response: dict) -> str:
+        input_type = tx_engine_response["input_type"] if type(tx_engine_response["input_type"]) == str else tx_engine_response["input_type"][0]
+        input_range = tx_engine_response["input_range"]
+
+        formatting = ['italic']
+
+        if input_type == "int":
+            hdr = f"Enter a number between ({input_range['min']} and {input_range['max']}):"
+
+        elif input_type == "none":
+            hdr = f"Press any key:"
+
+        elif input_type == "str":
+            hdr = "Enter a string: "
+
+        elif input_type == "affirmative":
+            hdr = "Enter y, n, yes, or no:"
+        elif input_type == "any":
+            hdr = "Press any key..."
+        else:
+            logger.error(f"Unexpected input type: {input_type}")
+            logger.debug(f"Failed frame: {str(tx_engine_response)}")
+            raise ValueError(f"Unexpected input type: {input_type}")
+
+        return format_string(hdr, formatting)
+
+    def display(self, tx_engine_response: dict):
         """
         Primitively print GET results
         """
@@ -49,38 +92,23 @@ class Viewer:
         def entity_to_str(entity_dict: dict[str, any]) -> str:
             return f"{entity_dict['name']}\n{entity_dict['primary_resource_name']}: [{entity_dict['primary_resource_val']}/{entity_dict['primary_resource_max']}]"
 
-        print(parse_content(value["components"]["content"]))
+        print(parse_content(tx_engine_response["components"]["content"]))
 
-        if "enemies" in value["components"]:
+        if "enemies" in tx_engine_response["components"]:
             print("ENEMIES")
-            for enemy in value["components"]["enemies"]:
+            for enemy in tx_engine_response["components"]["enemies"]:
                 print(entity_to_str(enemy))
 
-        if "allies" in value["components"]:
+        if "allies" in tx_engine_response["components"]:
             print("ALLIES")
-            for ally in value["components"]["allies"]:
+            for ally in tx_engine_response["components"]["allies"]:
                 print(entity_to_str(ally))
 
-        if "options" in value["components"] and type(value["components"]["options"]) == list:
-            for idx, opt in enumerate(value["components"]["options"]):
+        if "options" in tx_engine_response["components"] and type(tx_engine_response["components"]["options"]) == list:
+            for idx, opt in enumerate(tx_engine_response["components"]["options"]):
                 print(f"[{idx}] {parse_content(opt)}")
 
-        input_type = value["input_type"][0]
-        input_range = value["input_range"]
-
-        if input_type == "int":
-            print(f"Enter a number between ({input_range['min']} and {input_range['max']}):")
-
-        elif input_type == "none":
-            print(f"Press any key:")
-
-        elif input_type == "str":
-            print("Enter a string: ")
-
-        elif input_type == "affirmative":
-            print("Enter y, n, yes, or no:")
-        elif input_type == "any":
-            print("Press any key...")
+        print(self.get_text_header(tx_engine_response))
 
 
 class TextualViewer(App):
@@ -100,16 +128,8 @@ class TextualViewer(App):
 
 
 if __name__ == "__main__":
-
     viewer = Viewer()
+    viewer.start_session()
 
-    while True:
-        results = viewer.session.get(viewer.ip, verify=False)
-        viewer.display(results.json())
-        user_input = input()
-
-        put_results = viewer.session.put(viewer.ip, params={"user_input": user_input}, verify=False)
-
-
-    #viewer = TextualViewer()
-    #viewer.run()
+    # viewer = TextualViewer()
+    # viewer.run()
