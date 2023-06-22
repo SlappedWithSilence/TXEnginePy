85f79ae2bcfae0e1458ea756d9d02459c84d89ee
SlappedWithSilence <asigal55+02@gmail.com>
2023-06-22 12:17:49 -0700

    Initial commit

---
+++ test/systems/entity/test_resource_modifier.py
@@ -0,0 +1,24 @@
+from game.systems.entity.entities import CombatEntity
+from game.systems.item import item_manager
+
+
+def test_trivial():
+    """
+    Test that a modifier can be trivially attached without causing error
+    """
+    dummy_entity = CombatEntity(0, 0, name="Dummy", id=-110)
+    dummy_entity.resource_controller.attach_modifier(item_manager.get_instance(-114))
+
+    dummy_entity.resource_controller.attach_modifier(item_manager.get_instance(-115))
+    assert len(dummy_entity.resource_controller.get_modifiers("tr_health", int)) == 1
+
+
+def test_flat_attach():
+    dummy_entity = CombatEntity(0, 0, name="Dummy", id=-110)
+    tr_health_initial = dummy_entity.resource_controller.get_max("tr_health")
+
+    dummy_entity.resource_controller.attach_modifier(item_manager.get_instance(-115))
+    assert len(dummy_entity.resource_controller.get_modifiers("tr_health", int)) == 1
+
+    assert dummy_entity.resource_controller.get_max("tr_health") == \
+           tr_health_initial + item_manager.get_instance(-115).resource_modifiers["tr_health"]
