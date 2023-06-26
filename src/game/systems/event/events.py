10d04b152a257bb8c7322ebf2b082d45729e52a5
SlappedWithSilence <asigal55+02@gmail.com>
2023-06-26 12:08:07 -0700

    Refactored ConsumeItemEvent into separate file

--- src/game/systems/event/events.py
+++ src/game/systems/event/events.py
@@ -11,7 +11,6 @@ from game.structures.loadable import LoadableMixin
 from game.structures.loadable_factory import LoadableFactory
 from game.structures.messages import StringContent, ComponentFactory
 from game.structures.state_device import FiniteStateDevice
-from game.systems import item as item
 from game.systems.crafting import recipe_manager
 from game.systems.entity import entities as entities
 from game.systems.entity.resource import ResourceController
@@ -476,134 +475,40 @@ class ResourceEvent(Event):
         return ResourceEvent(json['resource_name'], json['quantity'], None, **kwargs)
 
 
-class ConsumeItemEvent(Event):
-    """Provides a standardized flow for prompting the user to optionally consume 'n' of a given item from his/her
-    inventory
-    """
+class TextEvent(Event):
 
-    class States(Enum):
-        DEFAULT = 0
-        PROMPT_CONSUME = 1
-        INSUFFICIENT_QUANTITY = 2
-        REFUSED_CONSUME = 3
-        ACCEPTED_CONSUME = 4
-        TERMINATE = -1
+    def __init__(self, text: str | list[str | StringContent]):
+        super().__init__(InputType.ANY, self.States, self.States.DEFAULT)
+        self.text = text
 
-    def __init__(self, item_id: int, item_quantity: int, callback: any = None):
-        super().__init__(InputType.SILENT, self.States, self.States.DEFAULT)
-        self.item_id = item_id
-        self.item_quantity = item_quantity
-        self.player_ref: entities.Player = game.cache.get_cache()['player']
-
-        if callable(callback):
-            self.callback = callback
-        elif callback is not None:
-            raise TypeError(f"Callback must be callable! Got {type(callback)} instead.")
-        else:
-            self.callback = None
-
-        # DEFAULT
-
-        @FiniteStateDevice.state_logic(self, self.States.DEFAULT, InputType.SILENT)
+        @FiniteStateDevice.state_logic(self, self.States.DEFAULT, InputType.ANY)
         def logic(_: any) -> None:
-            # Detect item quantities
-            if self.player_ref.inventory.total_quantity(self.item_id) < self.item_quantity:
-                self.set_state(self.States.INSUFFICIENT_QUANTITY)
-            else:
-                self.set_state(self.States.PROMPT_CONSUME)
+            self.set_state(self.States.TERMINATE)
 
         @FiniteStateDevice.state_content(self, self.States.DEFAULT)
-        def content():
-            return ComponentFactory.get()
-
-        # INSUFFICIENT_QUANTITY
-
-        @FiniteStateDevice.state_logic(self, self.States.INSUFFICIENT_QUANTITY, InputType.ANY)
-        def logic(_: any) -> None:
-            if self.callback:
-                self.callback(False)  # Transmit that the player failed to consume items to the callback
-            self.set_state(self.States.TERMINATE)
-
-        @FiniteStateDevice.state_content(self, self.States.INSUFFICIENT_QUANTITY)
-        def content():
-            return ComponentFactory.get([
-                "Insufficient quantity of ",
-                StringContent(value=f"{item.item_manager.get_name(self.item_id)}", formatting="item_name"),
-                "!"
-            ])
-
-        # PROMPT_CONSUME
-
-        @FiniteStateDevice.state_logic(self, self.States.PROMPT_CONSUME, InputType.AFFIRMATIVE)
-        def logic(user_input: bool) -> None:
-            if user_input:
-                self.set_state(self.States.ACCEPTED_CONSUME)
-            else:
-                self.set_state(self.States.REFUSED_CONSUME)
-
-        @FiniteStateDevice.state_content(self, self.States.PROMPT_CONSUME)
-        def content():
-            return ComponentFactory.get([
-                "Are you sure that you want to consume ",
-                StringContent(value=f"{self.item_quantity}x ", formatting="item_quantity"),
-                StringContent(value=f"{item.item_manager.get_name(self.item_id)}", formatting="item_name"),
-                "?"
-            ])
-
-        # ACCEPTED_CONSUME
-
-        @FiniteStateDevice.state_logic(self, self.States.ACCEPTED_CONSUME, InputType.ANY)
-        def logic(_: any) -> None:
-            assert self.player_ref.inventory.consume_item(self.item_id, self.item_quantity)
-            if self.callback:
-                self.callback(True)  # Transmit that the player successfully consumed items to the callback
-            self.set_state(self.States.TERMINATE)
-
-        @FiniteStateDevice.state_content(self, self.States.ACCEPTED_CONSUME)
-        def content():
-            return ComponentFactory.get([
-                "You consumed ",
-                StringContent(value=f"{self.item_quantity}x ", formatting="item_quantity"),
-                StringContent(value=f"{item.item_manager.get_name(self.item_id)}", formatting="item_name"),
-                "."
-            ])
-
-        # REFUSED_CONSUME
-
-        @FiniteStateDevice.state_logic(self, self.States.REFUSED_CONSUME, InputType.ANY)
-        def logic(_: any) -> None:
-            if self.callback:
-                self.callback(False)  # Transmit that the player did not consume items to the callback
-            self.set_state(self.States.TERMINATE)
-
-        @FiniteStateDevice.state_content(self, self.States.REFUSED_CONSUME)
-        def content():
-            return ComponentFactory.get([
-                "You refused to consume ",
-                StringContent(value=f"{self.item_quantity}x ", formatting="item_quantity"),
-                StringContent(value=f"{item.item_manager.get_name(self.item_id)}", formatting="item_name"),
-                "."
-            ])
+        def content() -> dict:
+            return ComponentFactory.get(
+                self.text
+            )
 
     @staticmethod
-    @cached([LoadableMixin.LOADER_KEY, "ConsumeItemEvent", LoadableMixin.ATTR_KEY])
     def from_json(json: dict[str, any]) -> any:
         """
-        Loads a ConsumeItemEvent object from a JSON blob.
+        Loads a TextEvent from a JSON blob
 
         Required JSON fields:
-        - item_id (int)
-        - item_quantity (int)
+        - text: str
+
+        Optional JSON fields:
+        - none
         """
 
-        required_fields = [
-            ("item_id", int),
-            ("item_quantity", int)
-        ]
+        required_fields = [("text", str)]
 
         LoadableFactory.validate_fields(required_fields, json)
 
-        if json['class'] != "ConsumeItemEvent":
-            raise ValueError()
+        return TextEvent(json["text"])
 
-        return ConsumeItemEvent(json['item_id'], json['item_quantity'])
+
+
+
