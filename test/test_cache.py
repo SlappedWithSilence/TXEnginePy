30effe73196c8fbf76793bdedd7299bcaf5d74ce
SlappedWithSilence <asigal55+02@gmail.com>
2023-07-18 11:57:40 -0700

    Added test cases for 'delete_element' and adjusted logic in other cases

--- test/test_cache.py
+++ test/test_cache.py
@@ -1,11 +1,18 @@
-from game.cache import from_cache, get_cache, get_loader, cache_element
+import game.cache
+from game.cache import from_cache, get_cache, get_loader, cache_element, delete_element
 
 
 def test_get_cache():
+    """
+    Test that the cache object can be retrieved
+    """
     assert type(get_cache()) == dict
 
 
 def test_from_cache():
+    """
+    Test that 'from_cache' can retrieve elements stored in the cache
+    """
     cache = get_cache()
     cache["element"] = 1
 
@@ -16,5 +23,38 @@ def test_from_cache():
     assert from_cache("root.branch") == "leaf"
 
 
+def test_delete_element():
+    """
+    Test that elements can be deleted from the cache correctly
+    """
+    cache = get_cache()
+    cache["element"] = 1
+
+    assert from_cache("element") == 1
+    delete_element("element")
+    assert from_cache("element") is None
+
+    cache["root"] = {"branch" : "leaf"}
+    assert from_cache("root.branch") == "leaf"
+
+    delete_element("root.branch")
+    assert from_cache("root.branch") is None
+
+
 def test_cache_element():
-    pass
+    """
+    Test that 'cache_element' can correctly store elements in the cache
+    """
+    delete_element("num")
+    delete_element("root.branch")
+    delete_element("element")
+
+    # Trivial case
+    assert from_cache("num") is None
+    cache_element("num", 1)
+    assert from_cache("num") == 1
+
+    # recursively cached
+    assert from_cache("root.branch") is None
+    cache_element("root.branch", "leaf")
+    assert from_cache("root.branch") == "leaf"
