550940b0690aa1ab3cb3b20c503bc1ee86ebacb0
SlappedWithSilence <asigal55+02@gmail.com>
2023-10-06 11:57:45 -0700

    Added tests for factory methods

--- test/systems/events/test_select_element_event.py
+++ test/systems/events/test_select_element_event.py
@@ -1,7 +1,8 @@
 import pytest
+from loguru import logger
 
 from game.cache import from_cache, from_storage
-from game.systems.event.select_element_event import SelectElementEvent
+from game.systems.event.select_element_event import SelectElementEvent, SelectElementEventFactory
 from game.systems.item import Usable
 from systems.events.event_tester import EventTester
 
@@ -86,3 +87,36 @@ def test_empty_filtered_collection():
     with pytest.raises(RuntimeError):
         tester = EventTester(e, [1], [])
         tester.run_tests()
+
+
+def test_factory_ability_selection_functional():
+    """Test that the SelectElementEventFactory's get_select_ability_event function returns a functional event"""
+    e = SelectElementEventFactory.get_select_ability_event(from_cache("managers.EntityManager").get_instance(-110))
+
+    links = e.link()
+
+    tester = EventTester(e, [1], [], show_frames=True)
+    tester.run_tests()
+
+    assert from_storage(links["selected_element"]) == "Test Ability 2"
+
+
+def test_factory_ability_selection_functional_castable():
+    """Test that the SelectElementEventFactory's get_select_ability_event function returns a functional event when
+    configured to filter for 'castable' abilities only.
+    """
+
+    # Modify entity to make Test Ability 2 not 'castable'
+    entity = from_cache("managers.EntityManager").get_instance(-110)  # Get a copy of the entity
+    entity.resource_controller.get_instance("tr_health").adjust(-1.0)  # Set health to 0
+    assert entity.resource_controller.get_value("tr_health") == 0  # Verify health is now 0
+    assert from_cache("managers.AbilityManager").get_instance("Test Ability 2").costs["tr_health"] > 0  # Verify ta_2 costs more than 0 health
+
+    event = SelectElementEventFactory.get_select_ability_event(entity, must_select=True, only_castable=True)
+
+    links = event.link()
+
+    tester = EventTester(event, [1], [], show_frames=True)
+    tester.run_tests()
+
+    assert from_storage(links["selected_element"]) == "Test Ability 3"
