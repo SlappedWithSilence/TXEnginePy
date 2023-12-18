1e7a8f085a4ce41af76dcfc3df8fd872b6ca3b65
SlappedWithSilence <asigal55+02@gmail.com>
2023-12-18 11:15:51 -0800

    Added logic to LootTable and LootableMixin

--- src/game/systems/item/loot.py
+++ src/game/systems/item/loot.py
@@ -5,12 +5,15 @@ from game.cache import from_cache
 
 
 class LootTable:
-    LOOT_TABLE_RESOLUTION = 1000  # The length of the loot table array
+    """
+    LootTable objects store organized data about the types of items that can drop and how many of them should drop.
+    """
+    ITEM_TABLE_RESOLUTION = 1000  # The length of the loot table array
     DROP_TABLE_RESOLUTION = 1000  # The length of the drop table array
 
-    def __init__(self, loot_probabilities: dict[int, float], drop_probabilities: dict[int, float]):
-        self._loot_table = self._generate_loot_table(loot_probabilities)  # Probability a drop is a specific item
-        self._drop_table = self._generate_drop_table(drop_probabilities)
+    def __init__(self, item_probabilities: dict[int, float], drop_probabilities: dict[int, float]):
+        self.item_table = self._generate_loot_table(item_probabilities)  # Probability a drop is a specific item
+        self.drop_table = self._generate_drop_table(drop_probabilities)
 
     @classmethod
     def _generate_loot_table(cls, loot_probabilities: dict[int, float]) -> list[int]:
@@ -25,6 +28,9 @@ class LootTable:
 
     @classmethod
     def _generate_drop_table(cls, drop_probabilities: dict[int, float]) -> list[int]:
+        """
+        Transform a dict of item probabilities into a list of item quantities.
+        """
         if drop_probabilities is None:
             return dict[0:1.0]
 
@@ -33,16 +39,24 @@ class LootTable:
 
 
 class LootableMixin(ABC):
+    """
+    A Mixin class that allows an object to support 'looting'.
 
-    def __init__(self, loot_table_id: int = None, loot_probabilities: dict[int, float] = None,
+    LootableMixin provides support for direct definition of LootTable objects or global referencing via the LootManager.
+    - To use direct definition, provide a value to `item_probabilities` and `drop_probabilities`.
+    - To use global referencing, provide a LootTable ID to `loot_table_id`. This should correspond to the ID of a
+        LootTable that has been registered with the LootManager.
+    """
+
+    def __init__(self, loot_table_id: int = None, item_probabilities: dict[int, float] = None,
                  drop_probabilities: dict[int, float] = None,
                  **kwargs):
         super(**kwargs)
 
-        if loot_table_id is not None and (loot_probabilities or drop_probabilities):
+        if loot_table_id is not None and (item_probabilities or drop_probabilities):
             raise ValueError("A LootableMixin cannot specify both a loot_table_id and a custom loot table!")
 
-        if loot_table_id is None and not (loot_probabilities and drop_probabilities):
+        if loot_table_id is None and not (item_probabilities and drop_probabilities):
             raise ValueError(
                 "A LootableMixin without a loot_table_id must provide both a loot_probabilities and drop_probabilities!"
             )
@@ -51,19 +65,26 @@ class LootableMixin(ABC):
             self._loot_table_id = loot_table_id  # The ID of a pre-made re-usable loot table defined in assets folder
 
         else:
-            self._loot_table_instance = LootTable(loot_probabilities, drop_probabilities)
+            self._loot_table_instance = LootTable(item_probabilities, drop_probabilities)
+
+    @property
+    def loot_table(self) -> LootTable:
+        if self._loot_table_id is not None:
+            return from_cache("managers.LootManager").get_instance(self._loot_table_id)
+        else:
+            return self._loot_table_instance
 
     def _get_num_drops(self) -> int:
         """
         Generate a random number to determine how many drops should be generated
         """
-        return self._drop_table[randint(0, len(self._drop_table) - 1)]
+        return self.loot_table.drop_table[randint(0, len(self.loot_table.drop_table) - 1)]
 
-    def _get_loot_instance(self) -> int:
+    def _get_item_from_pool(self) -> int:
         """
         Generate a random number to determine which item should drop
         """
-        return self._loot_table[randint(0, len(self._loot_table) - 1)]
+        return self.loot_table.item_table[randint(0, len(self.loot_table.item_table) - 1)]
 
     def get_loot(self) -> dict[int, int]:
         """
@@ -71,3 +92,14 @@ class LootableMixin(ABC):
 
         Each key represents an Item ID and the mapped values represents quantity
         """
+
+        drops: dict[int, int] = {}
+
+        for i in range(0, self._get_num_drops()):
+            item_id = self._get_item_from_pool()
+            if item_id not in drops:
+                drops[item_id] = 1
+            else:
+                drops[item_id] = drops[item_id] + 1
+
+        return drops
