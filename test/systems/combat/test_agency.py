cf964a2f913d28e817853b9f43b74869d446b0c9
SlappedWithSilence <asigal55+02@gmail.com>
2024-01-26 10:23:41 -0800

    Added tests for offensive_abilities, removed helper function

--- test/systems/combat/test_agency.py
+++ test/systems/combat/test_agency.py
@@ -7,16 +7,6 @@ from systems import TEST_PREFIX
 EXPECTED_PRIMARY_RESOURCE = F"{TEST_PREFIX}health"
 
 
-def fix_primary_resource(func):
-    def decorator(*args, **kwargs):
-        proper_res_name = get_config()["resources"]["primary_resource"]
-        get_config()["resources"]["primary_resource"] = EXPECTED_PRIMARY_RESOURCE
-        func(*args, **kwargs)
-        get_config()["resources"]["primary_resource"] = proper_res_name
-
-    return decorator
-
-
 def _get_intelligent_agent(inventory_contents: list[int] = None, abilities: list[str] = None,
                            resource_values: dict[str, int] = None) -> CombatEntity:
     entity = CombatEntity(name="Test Intelligent Agent", abilities=abilities, naive=False, id=-1)
@@ -51,8 +41,10 @@ def test_is_restorative_item(item_id: int, result: bool):
     assert CombatEntity._is_restorative_item(inst, EXPECTED_PRIMARY_RESOURCE) == result
 
 
+# List of Item IDs in, List of expected Item IDs out
 restorative_items_cases = [
     [[-120, -121, -119, -122, -123], [-122, -123]],
+    [[-120], []],
     [[], []]
 ]
 
@@ -68,3 +60,17 @@ def test_restorative_items(inventory_contents: list[int], expected_items: list[i
     assert set(ri) == set(expected_items)
 
     get_config()["resources"]["primary_resource"] = proper_res_name
+
+
+offensive_ability_cases =[
+    # For test abilities 1-6, only 1, 2, 4, 5 should pass as offensive abilities
+    [[f"{TEST_PREFIX}Ability {i+1}" for i in range(6)], [f"{TEST_PREFIX}Ability {i}" for i in [1, 2, 4, 5]]],
+    [[], []]
+]
+
+
+@pytest.mark.parametrize("ability_names, expected_names", offensive_ability_cases)
+def test_offensive_abilities(ability_names, expected_names):
+    entity = _get_intelligent_agent(abilities=ability_names)
+
+    assert set(expected_names) == set([a.name for a in entity.offensive_abilities])
