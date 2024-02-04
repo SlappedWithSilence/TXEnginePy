967b834f6f2cd283c4cb782ff039ba571d018428
SlappedWithSilence <asigal55+02@gmail.com>
2024-02-04 11:12:34 -0800

    Added import forwarding

---
+++ src/game/systems/entity/mixins/__init__.py
@@ -0,0 +1,7 @@
+from game.systems.entity.mixins.inventory_mixin import InventoryMixin
+from game.systems.entity.mixins.skill_mixin import SkillMixin
+from game.systems.entity.mixins.equipment_mixin import EquipmentMixin
+from game.systems.entity.mixins.ability_mixin import AbilityMixin
+from game.systems.entity.mixins.crafting_mixin import CraftingMixin
+from game.systems.entity.mixins.currency_mixin import CurrencyMixin
+from game.systems.entity.mixins.resource_mixin import ResourceMixin
