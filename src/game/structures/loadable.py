ee4b41447937dde71d4258d1d72355dc08ab1913
SlappedWithSilence <asigal55+02@gmail.com>
2024-05-09 13:13:30 -0700

    Fixed PEP8
    Removed placeholder generic loader call, added get_loader method to Mixin

--- src/game/structures/loadable.py
+++ src/game/structures/loadable.py
@@ -1,8 +1,11 @@
-from game.cache import cached
+from typing import Callable
 
 
 class LoadableMixin:
-    """An interface for classes that need to define logic for building an instance from JSON"""
+    """
+    An interface for classes that need to define logic for building an instance
+    from JSON.
+    """
 
     LOADER_KEY: str = 'loader'
     ATTR_KEY: str = "from_json"
@@ -10,7 +13,18 @@ class LoadableMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @staticmethod
-    @cached(["loader", "LoadableMixin", "from_json"])
-    def from_json(json: dict[str, any]) -> any:
-        raise NotImplementedError()
+    @classmethod
+    def get_loader(cls) -> Callable:
+        """
+        Fetch and return the JSON loader function for this class as stored in
+        the cache.
+
+        Returns: A callable that can be used to translate a JSON object into
+        an object of this type.
+
+        Raises:
+            KeyError:
+        """
+        from game.cache import from_cache
+
+        return from_cache([cls.LOADER_KEY, cls.__name__, cls.ATTR_KEY])
