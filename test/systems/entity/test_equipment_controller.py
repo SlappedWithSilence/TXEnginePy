086468273331de7a5e6c616c7db90efc5411720f
SlappedWithSilence <asigal55+02@gmail.com>
2024-06-17 22:46:06 -0700

    Added tests

---
+++ test/systems/entity/test_equipment_controller.py
@@ -0,0 +1,41 @@
+from game.cache import from_cache
+from game.systems.entity.entities import CombatEntity
+from game.systems.item.item import Equipment
+
+
+def _get_test_entity(equipment: list[int] = None) -> CombatEntity:
+    ce = CombatEntity(id=-1, name="Test")
+
+    for item_id in (equipment or []):
+        instance = from_cache("managers.ItemManager").get_instance(item_id)
+
+        assert isinstance(instance, Equipment)
+
+        ce.equipment_controller.equip(item_id)
+
+    return ce
+
+
+def _get_equipment(slot: str, dmg_buff: int = 0, dmg_res: int = 0,
+                   tags: dict[str, float] = None, iid: int = 0,
+                   name="Test Equipment"
+                   ) -> Equipment:
+
+    e = Equipment(name, iid, {}, "", "", slot, dmg_buff, dmg_res)
+
+    if tags:
+        e.tags = tags
+
+    return e
+
+
+def test_total_res_zero():
+    ce = _get_test_entity()
+
+    assert ce.equipment_controller.total_dmg_resistance == 0
+    assert len(ce.equipment_controller.total_tag_resistance) == 0
+
+def test_total_dmg_res():
+    ce = _get_test_entity([-114, -115])
+    assert ce.equipment_controller.total_dmg_resistance == 8
+    assert ce.equipment_controller.total_dmg_buff == 8
