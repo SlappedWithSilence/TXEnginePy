a734e56e61c2985c8955790aaafc8dc8318377e1
SlappedWithSilence <asigal55+02@gmail.com>
2024-06-27 16:24:13 -0700

    Improved testing logic and removed redundant importing logic

--- test/systems/utils.py
+++ test/systems/utils.py
@@ -1,10 +1,13 @@
 from contextlib import contextmanager
 from typing import Iterable
 
+from loguru import logger
+
 from game.systems.item import ItemManager
 from game.systems.item.item import Item
 from game.systems.entity import EntityManager
 from game.systems.entity.entities import CombatEntity
+from game.systems.combat import Ability, AbilityManager
 
 
 @contextmanager
@@ -14,13 +17,13 @@ def temporary_item(items: Iterable[Item]) -> ItemManager:
     with the global ItemManager instance and then deletes them when the scope
     exits.
     """
+    from game.systems.item import item_manager
 
     for item in items:
-        from game.systems.item import item_manager
+        logger.debug(f"Temporarily registering item with id: {item.id}")
         item_manager.register_item(item)
 
     try:
-        from game.systems.item import item_manager
         yield item_manager
 
     finally:
@@ -30,7 +33,6 @@ def temporary_item(items: Iterable[Item]) -> ItemManager:
         # This way, an exit that occurs due to a collision doesn't break the
         # Item Manager
 
-        from game.systems.item import item_manager
         for item in items:
 
             # Make sure item ID exists in manager
@@ -40,6 +42,7 @@ def temporary_item(items: Iterable[Item]) -> ItemManager:
                 if item_manager.get_ref(item.id).name == item.name:
                     # Delete from the manager permanently
                     del item_manager._manifest[item.id]
+                    logger.debug(f"De-registering item with id: {item.id}")
 
 
 @contextmanager
@@ -50,22 +53,21 @@ def temporary_entity(entities: Iterable[CombatEntity]) -> EntityManager:
     exits.
     """
 
+    from game.systems.entity import entity_manager
+
     for entity in entities:
-        from game.systems.entity import entity_manager
         entity_manager.register_entity(entity)
 
     try:
-        from game.systems.entity import entity_manager
         yield entity_manager
 
     finally:
 
-        # In order to "safely" delete items from the manager, check that the
-        # Item registered to that ID has the same name as the temporary Item.
+        # In order to "safely" delete entities from the manager, check that the
+        # Entity registered to that ID has the same name as the temporary Entity
         # This way, an exit that occurs due to a collision doesn't break the
-        # Item Manager
+        # EntityManager
 
-        from game.systems.entity import entity_manager
         for entity in entities:
 
             # Make sure item ID exists in manager
@@ -75,3 +77,29 @@ def temporary_entity(entities: Iterable[CombatEntity]) -> EntityManager:
                 if entity_manager.get_instance(entity.id).name == entity.name:
                     # Delete from the manager permanently
                     del entity_manager._manifest[entity.id]
+
+
+@contextmanager
+def temporary_ability(abilities: Iterable[Ability]) -> AbilityManager:
+    """
+    A context manager that temporarily registers a collection of Ability objects
+    with the global AbilityManager instance and then deletes them when the scope
+    exits.
+    """
+
+    from game.systems.combat import ability_manager
+
+    for ability in abilities:
+        ability_manager.register_ability(ability)
+
+    try:
+        yield ability_manager
+
+    finally:
+
+        for ability in abilities:
+
+            # Make sure item ID exists in manager
+            if ability_manager.is_id(ability.name):
+                # Delete from the manager permanently
+                del ability_manager._manifest[ability.name]
