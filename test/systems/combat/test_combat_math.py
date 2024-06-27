d645629784aa352704980a9038c840f9acfc9a83
SlappedWithSilence <asigal55+02@gmail.com>
2024-06-27 16:23:47 -0700

    Implemented several tag tests

--- test/systems/combat/test_combat_math.py
+++ test/systems/combat/test_combat_math.py
@@ -1,33 +1,90 @@
 import pytest
 
-from game.cache import from_cache
+from game.structures.enums import TargetMode
 from game.systems.combat import Ability
-from game.systems.entity.entities import CombatEntity
-
 from game.systems.combat.combat_engine.combat_helpers import \
     calculate_target_resistance
+from game.systems.entity.entities import CombatEntity
+from game.systems.item.item import Equipment
+from game.systems.inventory import EquipmentController
+from ..utils import temporary_item, temporary_entity, temporary_ability
 
-calculate_target_resistance_cases = [
 
-]
+def test_target_resistance_trivial():
+    """
+    Ensure that a basic zero-tag, zero-resistance scenario returns a proper
+    zero.
+    """
+    with temporary_ability([
+        Ability(name="temp_ab", description="", on_use="",
+                target_mode=TargetMode.SINGLE, damage=1)]
+    ) as ability_manager:
+        with temporary_entity(
+            [CombatEntity(id=-256, name="temp_ce")]
+        ) as entity_manager:
+            ab_inst = ability_manager.get_instance("temp_ab")
+            ce_inst = entity_manager.get_instance(-256)
+            calculated_res = calculate_target_resistance(ab_inst, ce_inst)
+
+            # There are no tags on anything, so resistance should be zero.
+            assert calculated_res == 0
 
 
-@pytest.mark.parametrize("ability_id, target, expected_resistance",
-                         calculate_target_resistance_cases)
-def test_calculate_target_resistance(ability_id: str,
-                                     target: CombatEntity,
-                                     expected_resistance: float):
+def test_target_resistance_single_tag():
+    """
+    For an ability with a single tag and a single piece of armor with resistance
+    verify that the total resistance is equal to the resistance of that one
+    piece of armor
     """
 
-    Args:
-        ability_id: the ID of the Ability to fetch
-        target: An instance of a CombatEntity
-        expected_resistance: expected result to compare against
+    with temporary_item([
+        Equipment("helm", -256, {-112: 0}, "", "", "head", 0, 0,
+                  tags={"tag_a": 0.33})
+    ]) as item_manager:
+        with temporary_ability([
+            Ability(name="temp_ab", description="", on_use="",
+                    target_mode=TargetMode.SINGLE, damage=10,
+                    tags={"tag_a": None})
+        ]) as ability_manager:
+            with temporary_entity([
+                CombatEntity(id=-256, name="temp_ce",
+                             equipment_controller=EquipmentController(
+                                 equipment=[-256]
+                             ))
+            ]) as entity_manager:
+                ab_inst = ability_manager.get_instance("temp_ab")
+                ce_inst = entity_manager.get_instance(-256)
+                calculated_res = calculate_target_resistance(ab_inst, ce_inst)
 
-    Returns: None
+                assert calculated_res == 0.33
+
+
+def test_target_resistance_multi_tag():
+    """
+    For an ability with a single tag and a single piece of armor with resistance
+    verify that the total resistance is equal to the resistance of that one
+    piece of armor
     """
 
-    instance: Ability = from_cache("managers.AbilityManager").get_instance(
-        ability_id)
+    with temporary_item([
+        Equipment("helm", -256, {-112: 0}, "", "", "head", 0, 0,
+                  tags={"tag_a": 0.33}),
+        Equipment("chest", -257, {-112: 0}, "", "", "chest", 0, 0,
+                  tags={"tag_a": 0.10})
+    ]) as item_manager:
+        with temporary_ability([
+            Ability(name="temp_ab", description="", on_use="",
+                    target_mode=TargetMode.SINGLE, damage=10,
+                    tags={"tag_a": None})
+        ]) as ability_manager:
+            with temporary_entity([
+                CombatEntity(id=-256, name="temp_ce",
+                             equipment_controller=EquipmentController(
+                                 equipment=[-256, -257]
+                             ))
+            ]) as entity_manager:
+                ab_inst = ability_manager.get_instance("temp_ab")
+                ce_inst = entity_manager.get_instance(-256)
+                calculated_res = calculate_target_resistance(ab_inst, ce_inst)
 
-    assert calculate_target_resistance(instance, target) == expected_resistance
+                assert calculated_res == 0.36
